b4761d8e974a45874a058bc815f2597fb57a437b
Romain Milbert <romain.milbert@gmail.com>
2017-10-20 09:40:53 +0200

    [Fix] Fixed quaternion matrix's signs, now rotates correctly on X axis

--- examples/main.cpp
+++ examples/main.cpp
@@ -9,18 +9,28 @@ int main() {
   const Raz::FragmentShader fragShader("../shaders/frag.glsl");
   const Raz::Material defaultMaterial(vertShader, fragShader);
 
-  const Raz::MeshPtr cube = std::make_shared<Raz::Mesh>("../assets/meshes/queen.off");
-  Raz::ModelPtr cubeModel = std::make_unique<Raz::Model>(cube);
-  cubeModel->setMaterial(defaultMaterial);
+  //const Raz::MeshPtr cubeMesh = std::make_shared<Raz::Mesh>("../assets/meshes/cube.obj");
+  const Raz::MeshPtr queenMesh = std::make_shared<Raz::Mesh>("../assets/meshes/queen.off");
+  //Raz::ModelPtr cubeModel = std::make_unique<Raz::Model>(cubeMesh);
+  Raz::ModelPtr queenModel = std::make_unique<Raz::Model>(queenMesh);
+  //cubeModel->setMaterial(defaultMaterial);
+  queenModel->setMaterial(defaultMaterial);
 
-  scene.addModel(std::move(cubeModel));
+  //auto cube = cubeModel.get();
+  auto queen = queenModel.get();
+
+  //scene.addModel(std::move(cubeModel));
+  scene.addModel(std::move(queenModel));
 
   // Uncommenting this call will display scene in wireframe
   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   while (window.run()) {
-    scene.getModels().front()->translate(0.005f, 0.005f, 0.f);
-    scene.getModels().front()->rotate(5.f, 0.f, 1.f, 0.f);
+    //cube->translate(0.04f, 0.002f, 0.f);
+    //cube->rotate(5.f, 0.f, 1.f, 0.f);
+
+    queen->translate(0.04f, 0.002f, 0.f);
+    queen->rotate(5.f, 0.f, 1.f, 0.f);
 
     scene.render();
   }
