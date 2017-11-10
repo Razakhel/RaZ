bd574154a491bf60cf94f1f712276b6b7d75712e
Romain Milbert <romain.milbert@gmail.com>
2017-11-11 00:31:28 +0100

    [Update] Externalised GL objects from Mesh.hpp

---
+++ include/RaZ/Render/GraphicObjects.hpp
@@ -0,0 +1,59 @@
+#include <vector>
+
+namespace Raz {
+
+class ElementBuffer {
+public:
+  ElementBuffer() { glGenBuffers(1, &m_index); }
+
+  GLuint getIndex() const { return m_index; }
+  const std::vector<unsigned int>& getIndices() const { return m_indices; }
+  std::vector<unsigned int>& getIndices() { return m_indices; }
+
+  void bind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_index); }
+  void unbind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); }
+
+  ~ElementBuffer() { glDeleteBuffers(1, &m_index); }
+
+private:
+  GLuint m_index;
+  std::vector<unsigned int> m_indices;
+};
+
+class VertexBuffer {
+public:
+  VertexBuffer() { glGenBuffers(1, &m_index); }
+
+  GLuint getIndex() const { return m_index; }
+  const std::vector<float>& getVertices() const { return m_vertices; }
+  std::vector<float>& getVertices() { return m_vertices; }
+
+  void bind() const { glBindBuffer(GL_ARRAY_BUFFER, m_index); }
+  void unbind() const { glBindBuffer(GL_ARRAY_BUFFER, 0); }
+
+  ~VertexBuffer() { glDeleteBuffers(1, &m_index); }
+
+private:
+  GLuint m_index;
+  std::vector<float> m_vertices;
+};
+
+class VertexArray {
+public:
+  VertexArray() { glGenVertexArrays(1, &m_index); }
+
+  GLuint getIndex() const { return m_index; }
+  const ElementBuffer& getEbo() const { return m_ebo; }
+  ElementBuffer& getEbo() { return m_ebo; }
+
+  void bind() const { glBindVertexArray(m_index); }
+  void unbind() const { glBindVertexArray(0); }
+
+  ~VertexArray() { glDeleteVertexArrays(1, &m_index); }
+
+private:
+  GLuint m_index;
+  ElementBuffer m_ebo;
+};
+
+} // namespace Raz
--- include/RaZ/Render/Mesh.hpp
+++ include/RaZ/Render/Mesh.hpp
@@ -5,71 +5,17 @@
 
 #include <memory>
 #include <string>
-#include <vector>
 
 #include "GL/glew.h"
 #include "RaZ/Math/Vector.hpp"
+#include "RaZ/Render/GraphicObjects.hpp"
 
 namespace Raz {
-
-class ElementBuffer {
-public:
-  ElementBuffer() { glGenBuffers(1, &m_index); }
-
-  GLuint getIndex() const { return m_index; }
-  const std::vector<std::size_t>& getIndices() const { return m_indices; }
-  std::vector<std::size_t>& getIndices() { return m_indices; }
-
-  void bind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_index); }
-  void unbind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); }
-
-  ~ElementBuffer() { glDeleteBuffers(1, &m_index); }
-
-private:
-  GLuint m_index;
-  std::vector<std::size_t> m_indices;
-};
-
-class VertexBuffer {
-public:
-  VertexBuffer() { glGenBuffers(1, &m_index); }
-
-  GLuint getIndex() const { return m_index; }
-  const std::vector<float>& getVertices() const { return m_vertices; }
-  std::vector<float>& getVertices() { return m_vertices; }
-
-  void bind() const { glBindBuffer(GL_ARRAY_BUFFER, m_index); }
-  void unbind() const { glBindBuffer(GL_ARRAY_BUFFER, 0); }
-
-  ~VertexBuffer() { glDeleteBuffers(1, &m_index); }
-
-private:
-  GLuint m_index;
-  std::vector<float> m_vertices;
-};
-
-class VertexArray {
-public:
-  VertexArray() { glGenVertexArrays(1, &m_index); }
-
-  GLuint getIndex() const { return m_index; }
-  const ElementBuffer& getEbo() const { return m_ebo; }
-  ElementBuffer& getEbo() { return m_ebo; }
-
-  void bind() const { glBindVertexArray(m_index); }
-  void unbind() const { glBindVertexArray(0); }
-
-  ~VertexArray() { glDeleteVertexArrays(1, &m_index); }
-
-private:
-  GLuint m_index;
-  ElementBuffer m_ebo;
-};
 
 class Mesh {
 public:
   Mesh() = default;
-  Mesh(const std::string& fileName) { import(fileName); }
+  Mesh(const std::string& fileName) { importFromFile(fileName); }
 
   const VertexArray& getVao() const { return m_vao; }
   VertexArray& getVao() { return m_vao; }
@@ -78,9 +24,9 @@ public:
   const ElementBuffer& getEbo() const { return m_vao.getEbo(); }
   ElementBuffer& getEbo() { return m_vao.getEbo(); }
   std::size_t getVertexCount() const { return m_vbo.getVertices().size(); }
-  std::size_t getIndexCount() const { return m_vao.getEbo().getIndices().size(); }
+  std::size_t getIndexCount() const { return getEbo().getIndices().size(); }
 
-  void import(const std::string& fileName);
+  void importFromFile(const std::string& fileName);
   void load();
   void draw() const;
 
@@ -90,6 +36,8 @@ private:
 
   VertexArray m_vao;
   VertexBuffer m_vbo;
+  bool m_hasTexcoords = false;
+  bool m_hasNormals = false;
 };
 
 using MeshPtr = std::shared_ptr<Mesh>;
