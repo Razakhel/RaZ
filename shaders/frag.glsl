8fd794ac916f5076fc766407377ea936b1217716
Razakhel <romain.milbert@gmail.com>
2017-11-29 22:52:25 +0100

    [Update] Implemented Blinn-Phong shading model [Update] Light impact's calculation has been moved into the frag shader

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,10 +1,53 @@
 #version 330 core
 
+#define MAX_LIGHT_COUNT 10
+
+struct Light {
+  vec4 position;
+  vec3 direction;
+  vec3 color;
+  float angle;
+};
+
+uniform uint uniLightCount;
+uniform Light uniLights[MAX_LIGHT_COUNT];
+
 uniform sampler2D uniTexture;
 
-in vec2 fragTexcoords;
-in float fragLightHitAngle;
+in MeshInfo {
+  vec3 vertPosition;
+  vec2 vertTexcoords;
+  vec3 vertNormals;
+} fragMeshInfo;
 
 void main() {
-  gl_FragColor = fragLightHitAngle * texture(uniTexture, fragTexcoords);
+  vec3 norm = normalize(fragMeshInfo.vertNormals);
+
+  vec3 color = texture(uniTexture, fragMeshInfo.vertTexcoords).rgb;
+
+  vec3 ambient = color * 0.05;
+  vec3 diffuse = vec3(0.0);
+  vec3 specular = vec3(0.0);
+
+  for (uint lightIndex = 0u; lightIndex < uniLightCount; ++lightIndex) {
+    vec3 lightPos = uniLights[lightIndex].position.xyz;
+
+    // Diffuse
+    vec3 lightDir;
+
+    if (uniLights[lightIndex].position.w == 0.0) {
+      lightDir = normalize(lightPos - fragMeshInfo.vertPosition);
+    } else {
+      lightDir = uniLights[lightIndex].direction;
+    }
+
+    diffuse += max(dot(lightDir, norm), 0.0) * color;
+
+    // Specular
+    vec3 viewDir = normalize(-fragMeshInfo.vertPosition);
+    vec3 halfDir = normalize(lightDir + viewDir);
+    specular += uniLights[lightIndex].color * pow(max(dot(norm, halfDir), 0.0), 32.0);
+  }
+
+  gl_FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,33 +1,23 @@
 #version 330 core
 
-#define MAX_LIGHT_COUNT 10
-
 layout (location = 0) in vec3 vertPosition;
 layout (location = 1) in vec2 vertTexcoords;
 layout (location = 2) in vec3 vertNormals;
 
-struct Light {
-  vec4 position;
-  vec3 direction;
-  vec3 color;
-  float angle;
-};
-
-uniform uint uniLightCount;
-uniform Light uniLights[MAX_LIGHT_COUNT];
 uniform mat4 uniMvpMatrix;
 
-out vec2 fragTexcoords;
-out float fragLightHitAngle;
+out MeshInfo {
+  vec3 vertPosition;
+  vec2 vertTexcoords;
+  vec3 vertNormals;
+} fragMeshInfo;
 
 void main() {
-  gl_Position = vec4(vertPosition.xyz, 1.0) * uniMvpMatrix;
-  fragTexcoords = vertTexcoords;
+  vec4 pos = uniMvpMatrix * vec4(vertPosition.xyz, 1.0);
 
-  if (uniLightCount > 0u) {
-    for (uint lightIndex = 0u; lightIndex < uniLightCount; ++lightIndex)
-      fragLightHitAngle /*+*/= clamp(dot(vertNormals, uniLights[lightIndex].direction), 0, 1);
-  } else {
-    fragLightHitAngle = 1;
-  }
+  fragMeshInfo.vertPosition = pos.xyz;
+  fragMeshInfo.vertTexcoords = vertTexcoords;
+  fragMeshInfo.vertNormals = vertNormals;
+
+  gl_Position = pos;
 }
