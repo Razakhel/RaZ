a3675caf39bdfbb096bafd8cd7fe2aeef07a3b28
Razakhel <romain.milbert@gmail.com>
2017-11-29 20:40:58 +0100

    [Update] A Light now inherits from Transform in order to be moved [Update] *Light classes can return a vec4, 4th elt indicates direction

--- include/RaZ/Render/Light.hpp
+++ include/RaZ/Render/Light.hpp
@@ -3,15 +3,17 @@
 #ifndef RAZ_LIGHT_HPP
 #define RAZ_LIGHT_HPP
 
+#include <memory>
+
 #include "RaZ/Math/Matrix.hpp"
 #include "RaZ/Math/Vector.hpp"
+#include "RaZ/Math/Transform.hpp"
 
 namespace Raz {
 
-class Light {
+class Light : public Transform {
 public:
-  const Vec4f& getPosition() const { return m_position; }
-  Vec4f& getPosition() { return m_position; }
+  virtual Vec4f getHomogeneousPosition() const = 0;
   const Vec3f& getDirection() const { return m_direction; }
   Vec3f& getDirection() { return m_direction; }
   const Vec3f& getColor() const { return m_color; }
@@ -19,33 +21,39 @@ public:
   float getAngle() const { return m_angle; }
 
 protected:
-  Light(const Vec4f& position, const Vec3f& color) : m_position(position), m_color(color) {}
-  Light(const Vec4f& position, const Vec3f& direction, const Vec3f& color)
-    : m_position(position), m_direction(direction), m_color(color) {}
-  Light(const Vec4f& position, const Vec3f& direction, const Vec3f& color, float angle)
-    : m_position(position), m_direction(direction), m_color(color), m_angle{ angle } {}
+  Light(const Vec3f& position, const Vec3f& color) : m_color{ color } { m_position = position; }
+  Light(const Vec3f& position, const Vec3f& direction, const Vec3f& color)
+    : m_direction{ direction }, m_color{ color } { m_position = position; }
+  Light(const Vec3f& position, const Vec3f& direction, const Vec3f& color, float angle)
+    : m_direction{ direction }, m_color{ color }, m_angle{ angle } { m_position = position; }
 
-  Vec4f m_position {};
   Vec3f m_direction {};
   Vec3f m_color {};
   float m_angle = 0.f;
 };
 
+using LightPtr = std::unique_ptr<Light>;
+
 class PointLight : public Light {
 public:
-  PointLight(const Vec4f& position, const Vec3f& color) : Light(position, color) {}
+  PointLight(const Vec3f& position, const Vec3f& color) : Light(position, color) {}
+
+  Vec4f getHomogeneousPosition() const override { return Vec4f(m_position, 0.f); }
 };
 
 class DirectionalLight : public Light {
 public:
-  DirectionalLight(const Vec4f& position, const Vec3f& direction, const Vec3f& color)
-    : Light(position, direction, color) {}
+  DirectionalLight(const Vec3f& position, const Vec3f& direction, const Vec3f& color) : Light(position, direction, color) {}
+
+  Vec4f getHomogeneousPosition() const override { return Vec4f(m_position, 1.f); }
 };
 
 class SpotLight : public Light {
 public:
-  SpotLight(const Vec4f& position, const Vec3f& direction, const Vec3f& color, float angle)
+  SpotLight(const Vec3f& position, const Vec3f& direction, const Vec3f& color, float angle)
     : Light(position, direction, color, angle) {}
+
+  Vec4f getHomogeneousPosition() const override { return Vec4f(m_position, 1.f); }
 };
 
 } // namespace Raz
