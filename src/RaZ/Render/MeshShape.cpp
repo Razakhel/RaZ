a62722b0596b8e01b5c7e392a6d25736cd407e60
Romain Milbert <romain.milbert@gmail.com>
2017-12-02 00:16:19 +0100

    [Update] Shapes' normals are now computed automatically

--- src/RaZ/Render/MeshShape.cpp
+++ src/RaZ/Render/MeshShape.cpp
@@ -3,22 +3,24 @@
 namespace Raz {
 
 Mesh::Mesh(const Vec3f& pos1, const Vec3f& pos2, const Vec3f& pos3) {
-  // TODO: Naive normal creation, need to compute them
+  // TODO: check if vertices are defined counterclockwise
 
   Vertex vert1 {};
   vert1.positions = pos1;
   vert1.texcoords = Vec2f({ 0.f, 1.f });
-  vert1.normals = Vec3f({ 0.f, 1.f, 0.f });
 
   Vertex vert2 {};
   vert2.positions = pos2;
   vert2.texcoords = Vec2f({ 0.5f, 0.f });
-  vert2.normals = Vec3f({ 0.f, 1.f, 0.f });
 
   Vertex vert3 {};
   vert3.positions = pos3;
   vert3.texcoords = Vec2f({ 1.f, 1.f });
-  vert3.normals = Vec3f({ 0.f, 1.f, 0.f });
+
+  // Computing normals
+  vert1.normals = (vert1.positions - vert2.positions).cross(vert1.positions - vert3.positions);
+  vert2.normals = (vert2.positions - vert3.positions).cross(vert2.positions - vert1.positions);
+  vert3.normals = (vert3.positions - vert1.positions).cross(vert3.positions - vert2.positions);
 
   m_vbo.getVertices().resize(3);
   m_vao.getEbo().getIndices().resize(3);
@@ -27,35 +29,37 @@ Mesh::Mesh(const Vec3f& pos1, const Vec3f& pos2, const Vec3f& pos3) {
   m_vbo.getVertices()[1] = vert2;
   m_vbo.getVertices()[2] = vert3;
 
-  m_vao.getEbo().getIndices()[0] = 0;
-  m_vao.getEbo().getIndices()[1] = 1;
+  m_vao.getEbo().getIndices()[0] = 1;
+  m_vao.getEbo().getIndices()[1] = 0;
   m_vao.getEbo().getIndices()[2] = 2;
 
   load();
 }
 
 Mesh::Mesh(const Vec3f& posTopLeft, const Vec3f& posTopRight, const Vec3f& posBottomRight, const Vec3f& posBottomLeft) {
-  // TODO: Naive normal creation, need to compute them
+  // TODO: check if vertices are defined counterclockwise
 
   Vertex topLeft {};
   topLeft.positions = posTopLeft;
   topLeft.texcoords = Vec2f({ 0.f, 0.f });
-  topLeft.normals = Vec3f({ 0.f, 1.f, 0.f });
 
   Vertex topRight {};
   topRight.positions = posTopRight;
   topRight.texcoords = Vec2f({ 1.f, 0.f });
-  topRight.normals = Vec3f({ 0.f, 1.f, 0.f });
 
   Vertex bottomRight {};
   bottomRight.positions = posBottomRight;
   bottomRight.texcoords = Vec2f({ 1.f, 1.f });
-  bottomRight.normals = Vec3f({ 0.f, 1.f, 0.f });
 
   Vertex bottomLeft {};
   bottomLeft.positions = posBottomLeft;
   bottomLeft.texcoords = Vec2f({ 0.f, 1.f });
-  bottomLeft.normals = Vec3f({ 0.f, 1.f, 0.f });
+
+  // Computing normals
+  topLeft.normals = (topLeft.positions - topRight.positions).cross(topLeft.positions - bottomLeft.positions).normalize();
+  topRight.normals = (topRight.positions - bottomRight.positions).cross(topRight.positions - topLeft.positions).normalize();
+  bottomRight.normals = (bottomRight.positions - bottomLeft.positions).cross(bottomRight.positions - topRight.positions).normalize();
+  bottomLeft.normals = (bottomLeft.positions - topLeft.positions).cross(bottomLeft.positions - bottomRight.positions).normalize();
 
   m_vbo.getVertices().resize(4);
   m_vao.getEbo().getIndices().resize(6);
