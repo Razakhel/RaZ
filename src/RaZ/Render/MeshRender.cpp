5b460af06dc165959686504b30e226634527cd40
Romain Milbert <romain.milbert@gmail.com>
2017-12-21 11:44:05 +0100

    [Update] Loading & rendering a Mesh now loads & renders each submesh

--- src/RaZ/Render/MeshRender.cpp
+++ src/RaZ/Render/MeshRender.cpp
@@ -2,47 +2,17 @@
 
 namespace Raz {
 
-void Mesh::load() {
-  m_vao.bind();
-
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-               sizeof(getEbo().getIndices().front()) * getEbo().getIndices().size(),
-               getEbo().getIndices().data(),
-               GL_STATIC_DRAW);
-
-  m_vbo.bind();
-  glBufferData(GL_ARRAY_BUFFER,
-               sizeof(m_vbo.getVertices().front()) * m_vbo.getVertices().size(),
-               m_vbo.getVertices().data(),
-               GL_STATIC_DRAW);
-
-  const uint8_t stride = sizeof(m_vbo.getVertices().front());
-
-  glVertexAttribPointer(0, 3,
-                        GL_FLOAT, GL_FALSE,
-                        stride,
-                        nullptr);
-  glEnableVertexAttribArray(0);
-
-  glVertexAttribPointer(1, 2,
-                        GL_FLOAT, GL_FALSE,
-                        stride,
-                        reinterpret_cast<void*>(3 * sizeof(m_vbo.getVertices().front().positions.getData().front())));
-  glEnableVertexAttribArray(1);
-
-  glVertexAttribPointer(2, 3,
-                        GL_FLOAT, GL_FALSE,
-                        stride,
-                        reinterpret_cast<void*>(5 * sizeof(m_vbo.getVertices().front().normals.getData().front())));
-  glEnableVertexAttribArray(2);
-
-  m_vbo.unbind();
-  m_vao.unbind();
+void Mesh::load() const {
+  for (const auto& submesh : m_submeshes)
+    submesh->load();
 }
 
 void Mesh::draw() const {
-  m_vao.bind();
-  glDrawElements(GL_TRIANGLES, getIndexCount(), GL_UNSIGNED_INT, nullptr);
+  for (const auto& submesh : m_submeshes) {
+    glActiveTexture(GL_TEXTURE0);
+    m_materials[submesh->getMaterialIndex()].getTexture()->bind();
+    submesh->draw();
+  }
 }
 
 } // namespace Raz
