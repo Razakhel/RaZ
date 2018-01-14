77d46a5ee8f089fcae5b5be0c035274b7ca83dae
Romain Milbert <romain.milbert@gmail.com>
2018-01-14 19:43:38 +0100

    [Update] Added a normal buffer to Framebuffer

--- include/RaZ/Render/Framebuffer.hpp
+++ include/RaZ/Render/Framebuffer.hpp
@@ -15,19 +15,21 @@ public:
   Framebuffer(unsigned int width, unsigned int height);
 
   GLuint getIndex() const { return m_index; }
-  const TexturePtr& getColorBuffer() const { return m_colorBuffer; }
   const TexturePtr& getDepthBuffer() const { return m_depthBuffer; }
+  const TexturePtr& getColorBuffer() const { return m_colorBuffer; }
+  const TexturePtr& getNormalBuffer() const { return m_normalBuffer; }
 
   void bind() const;
-  void unbind() const;
+  void unbind() const { glBindFramebuffer(GL_FRAMEBUFFER, 0); }
   void display() const;
 
   ~Framebuffer() { glDeleteFramebuffers(1, &m_index); }
 
 private:
   GLuint m_index;
-  TexturePtr m_colorBuffer;
   TexturePtr m_depthBuffer;
+  TexturePtr m_colorBuffer;
+  TexturePtr m_normalBuffer;
   MeshPtr m_viewport;
   ShaderProgram m_program;
 };
--- src/RaZ/Render/Framebuffer.cpp
+++ src/RaZ/Render/Framebuffer.cpp
@@ -6,19 +6,26 @@
 namespace Raz {
 
 Framebuffer::Framebuffer(unsigned int width, unsigned int height) : m_program(VertexShader("../shaders/framebufferVert.glsl"),
-                                                                              FragmentShader("../shaders/framebufferFrag.glsl")) {
-  m_colorBuffer = std::make_shared<Texture>(width, height);
+                                                                              FragmentShader("../shaders/ssr.glsl")) {
   m_depthBuffer = std::make_shared<Texture>(width, height, true);
+  m_colorBuffer = std::make_shared<Texture>(width, height);
+  m_normalBuffer = std::make_shared<Texture>(width, height);
   m_viewport = std::make_shared<Mesh>(Vec3f({ -1.f, 1.f, 0.f }),
                                       Vec3f({ 1.f, 1.f, 0.f }),
                                       Vec3f({ 1.f, -1.f, 0.f }),
                                       Vec3f({ -1.f, -1.f, 0.f }));
-  m_viewport->addMaterial(std::make_unique<Material>(m_colorBuffer));
   glGenFramebuffers(1, &m_index);
 
+  m_program.use();
+
+  glUniform1i(glGetUniformLocation(m_program.getIndex(), "uniSceneDepthBuffer"), 0);
+  glUniform1i(glGetUniformLocation(m_program.getIndex(), "uniSceneColorBuffer"), 1);
+  glUniform1i(glGetUniformLocation(m_program.getIndex(), "uniSceneNormalBuffer"), 2);
+
   bind();
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorBuffer->getIndex(), 0);
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depthBuffer->getIndex(), 0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorBuffer->getIndex(), 0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, m_normalBuffer->getIndex(), 0);
 
   if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
     std::cerr << "Error: Framebuffer is not complete." << std::endl;
@@ -31,15 +38,18 @@ void Framebuffer::bind() const {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-void Framebuffer::unbind() const {
-  glBindFramebuffer(GL_FRAMEBUFFER, 0);
-}
-
 void Framebuffer::display() const {
   glClear(GL_COLOR_BUFFER_BIT);
 
   m_program.use();
+
+  glActiveTexture(GL_TEXTURE0);
+  m_depthBuffer->bind();
+  glActiveTexture(GL_TEXTURE1);
   m_colorBuffer->bind();
+  glActiveTexture(GL_TEXTURE2);
+  m_normalBuffer->bind();
+
   m_viewport->draw();
 }
 
