fc635c916bf9aef166290e08b74404a57dd2bad5
Razakhel <romain.milbert@gmail.com>
2018-03-12 14:13:48 +0100

    [Fix] Corrected lookAt now that vector's operators are fixed

--- src/RaZ/Render/Camera.cpp
+++ src/RaZ/Render/Camera.cpp
@@ -3,7 +3,7 @@
 namespace Raz {
 
 Mat4f Camera::computePerspectiveMatrix() const {
-  const float halfFovTangent = std::tan(m_fieldOfView / 2);
+  const float halfFovTangent = std::tan(m_fieldOfView / 2.f);
   const float planeDist = m_farPlane - m_nearPlane;
   const float planeMult = m_farPlane * m_nearPlane;
   const float fovRatio = m_frameRatio * halfFovTangent;
@@ -17,14 +17,14 @@ Mat4f Camera::computePerspectiveMatrix() const {
 }
 
 Mat4f Camera::lookAt(const Vec3f& target, const Vec3f& orientation) const {
-  const Vec3f axisZ((target - m_position).normalize());
-  const Vec3f axisX(axisZ.cross(orientation).normalize());
-  const Vec3f axisY(axisX.cross(axisZ));
+  const Vec3f zAxis((m_position - target).normalize());
+  const Vec3f xAxis(zAxis.cross(orientation).normalize());
+  const Vec3f yAxis(xAxis.cross(zAxis));
 
-  const Mat4f res({{               axisX[0],               axisY[0],             -axisZ[0], 0.f },
-                   {               axisX[1],               axisY[1],             -axisZ[1], 0.f },
-                   {               axisX[2],               axisY[2],             -axisZ[2], 0.f },
-                   { axisX.dot(-m_position), axisY.dot(-m_position), axisZ.dot(m_position), 1.f }});
+  const Mat4f res({{               xAxis[0],               yAxis[0],             -zAxis[0], 0.f },
+                   {               xAxis[1],               yAxis[1],             -zAxis[1], 0.f },
+                   {               xAxis[2],               yAxis[2],             -zAxis[2], 0.f },
+                   { xAxis.dot(-m_position), yAxis.dot(-m_position), zAxis.dot(m_position), 1.f }});
 
   return res;
 }
