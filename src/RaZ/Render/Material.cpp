9264beb5a87ec6a00812f4bbd15e52a418898cbf
Razakhel <romain.milbert@gmail.com>
2018-04-04 12:59:27 +0200

    [Update] A CT Material is now sending its base color to the program [Update] Recovering a Material from its preset value is available [Update] Added multiple material presets

--- src/RaZ/Render/Material.cpp
+++ src/RaZ/Render/Material.cpp
@@ -2,6 +2,39 @@
 
 namespace Raz {
 
+namespace {
+
+const std::unordered_map<MaterialPreset, std::pair<Vec3f, float>> MATERIAL_PRESET_PARAMS = {
+  { MaterialPreset::CHARCOAL,  { Vec3f(0.02f), 0.f } },
+  { MaterialPreset::GRASS,     { Vec3f(0.21f), 0.f } },
+  { MaterialPreset::SAND,      { Vec3f(0.36f), 0.f } },
+  { MaterialPreset::ICE,       { Vec3f(0.56f), 0.f } },
+  { MaterialPreset::SNOW,      { Vec3f(0.81f), 0.f } },
+
+  { MaterialPreset::IRON,      { Vec3f({ 0.560f, 0.570f, 0.580f }), 1.f } },
+  { MaterialPreset::SILVER,    { Vec3f({ 0.972f, 0.960f, 0.915f }), 1.f } },
+  { MaterialPreset::ALUMINIUM, { Vec3f({ 0.913f, 0.921f, 0.925f }), 1.f } },
+  { MaterialPreset::GOLD,      { Vec3f({ 1.000f, 0.766f, 0.336f }), 1.f } },
+  { MaterialPreset::COPPER,    { Vec3f({ 0.955f, 0.637f, 0.538f }), 1.f } },
+  { MaterialPreset::CHROMIUM,  { Vec3f({ 0.550f, 0.556f, 0.554f }), 1.f } },
+  { MaterialPreset::NICKEL,    { Vec3f({ 0.660f, 0.609f, 0.526f }), 1.f } },
+  { MaterialPreset::TITANIUM,  { Vec3f({ 0.542f, 0.497f, 0.449f }), 1.f } },
+  { MaterialPreset::COBALT,    { Vec3f({ 0.662f, 0.655f, 0.634f }), 1.f } },
+  { MaterialPreset::PLATINUM,  { Vec3f({ 0.672f, 0.637f, 0.585f }), 1.f } }
+};
+
+}
+
+MaterialPtr Material::recoverMaterial(MaterialPreset preset, float roughnessFactor) {
+  const auto& materialParamsIter = MATERIAL_PRESET_PARAMS.find(preset);
+
+  if (materialParamsIter == MATERIAL_PRESET_PARAMS.end())
+    throw std::invalid_argument("Error: Couldn't find the given material preset");
+
+  const auto& materialParams = materialParamsIter->second;
+  return std::make_unique<MaterialCookTorrance>(materialParams.first, materialParams.second, roughnessFactor);
+}
+
 void MaterialStandard::initTextures(const ShaderProgram& program) const {
   const std::string locationBase = "uniMaterial.";
 
@@ -68,9 +101,11 @@ void MaterialCookTorrance::initTextures(const ShaderProgram& program) const {
 void MaterialCookTorrance::bindAttributes(const ShaderProgram& program) const {
   const std::string locationBase = "uniMaterial.";
 
+  const std::string baseColorLocation = locationBase + "baseColor";
   const std::string metallicFactorLocation = locationBase + "metallicFactor";
   const std::string roughnessFactorLocation = locationBase + "roughnessFactor";
 
+  program.sendUniform(baseColorLocation, m_baseColor);
   program.sendUniform(metallicFactorLocation, m_metallicFactor);
   program.sendUniform(roughnessFactorLocation, m_roughnessFactor);
 
