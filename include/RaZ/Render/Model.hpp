90fcce79a93239b8c685e7f172e2c73114ec625a
Razakhel <romain.milbert@gmail.com>
2018-04-23 14:25:11 +0200

    [Delete] Removed now useless quad mesh constructor from Model [Update] Removed cloning function from Model (MeshPtr is unique_ptr now)

--- include/RaZ/Render/Model.hpp
+++ include/RaZ/Render/Model.hpp
@@ -12,16 +12,12 @@ namespace Raz {
 
 class Model : public Transform {
 public:
-  Model(const Vec3f& topLeftPos, const Vec3f& topRightPos,
-        const Vec3f& bottomRightPos, const Vec3f& bottomLeftPos,
-        MaterialPtr material);
   explicit Model(MeshPtr mesh) : m_mesh{ std::move(mesh) } {}
 
-  const MeshPtr getMesh() const { return m_mesh; }
+  const MeshPtr& getMesh() const { return m_mesh; }
 
-  std::unique_ptr<Model> clone() const { return std::make_unique<Model>(*this); }
-  void load(const ShaderProgram& program) const { m_mesh->load(program); }
-  void draw(const ShaderProgram& program) const { m_mesh->draw(program); }
+  void load(const ShaderProgram& program) const { m_mesh->load(&program); }
+  void draw(const ShaderProgram& program) const { m_mesh->draw(&program); }
 
 private:
   MeshPtr m_mesh;
