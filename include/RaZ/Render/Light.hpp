a074998b941b6ac29fffcb22d33ab1440ac5f584
Razakhel <romain.milbert@gmail.com>
2018-05-18 10:52:21 +0200

    [Update] Lights now have an associated energy [Update] Added default values for lights params (white color, energy 1) [Update] Scene now sends lights' energy to shaders

--- include/RaZ/Render/Light.hpp
+++ include/RaZ/Render/Light.hpp
@@ -17,40 +17,43 @@ public:
   Vec3f& getDirection() { return m_direction; }
   const Vec3f& getColor() const { return m_color; }
   Vec3f& getColor() { return m_color; }
+  float getEnergy() const { return m_energy; }
   float getAngle() const { return m_angle; }
 
 protected:
-  Light(const Vec3f& position, const Vec3f& color) : m_color{ color } { m_position = position; }
-  Light(const Vec3f& position, const Vec3f& direction, const Vec3f& color)
-    : m_direction{ direction }, m_color{ color } { m_position = position; }
-  Light(const Vec3f& position, const Vec3f& direction, const Vec3f& color, float angle)
-    : m_direction{ direction }, m_color{ color }, m_angle{ angle } { m_position = position; }
+  Light(const Vec3f& position, float energy, const Vec3f& color) : m_energy{ energy }, m_color{ color } { m_position = position; }
+  Light(const Vec3f& position, const Vec3f& direction, float energy, const Vec3f& color)
+    : m_direction{ direction }, m_energy{ energy }, m_color{ color } { m_position = position; }
+  Light(const Vec3f& position, const Vec3f& direction, float angle, float energy, const Vec3f& color)
+    : m_direction{ direction }, m_angle{ angle }, m_energy{ energy }, m_color{ color } { m_position = position; }
 
   Vec3f m_direction {};
+  float m_angle  = 0.f;
+  float m_energy = 1.f;
   Vec3f m_color {};
-  float m_angle = 0.f;
 };
 
 using LightPtr = std::unique_ptr<Light>;
 
 class PointLight : public Light {
 public:
-  PointLight(const Vec3f& position, const Vec3f& color) : Light(position, color) {}
+  PointLight(const Vec3f& position, float energy = 1.f, const Vec3f& color = Vec3f(1.f)) : Light(position, energy, color) {}
 
   Vec4f getHomogeneousPosition() const override { return Vec4f(m_position, 1.f); }
 };
 
 class DirectionalLight : public Light {
 public:
-  DirectionalLight(const Vec3f& position, const Vec3f& direction, const Vec3f& color) : Light(position, direction, color) {}
+  DirectionalLight(const Vec3f& position, const Vec3f& direction, float energy = 1.f, const Vec3f& color = Vec3f(1.f))
+    : Light(position, direction, energy, color) {}
 
   Vec4f getHomogeneousPosition() const override { return Vec4f(m_position, 0.f); }
 };
 
 class SpotLight : public Light {
 public:
-  SpotLight(const Vec3f& position, const Vec3f& direction, const Vec3f& color, float angle)
-    : Light(position, direction, color, angle) {}
+  SpotLight(const Vec3f& position, const Vec3f& direction, float angle, float energy = 1.f, const Vec3f& color = Vec3f(1.f))
+    : Light(position, direction, angle, energy, color) {}
 
   Vec4f getHomogeneousPosition() const override { return Vec4f(m_position, 1.f); }
 };
--- src/RaZ/Render/Scene.cpp
+++ src/RaZ/Render/Scene.cpp
@@ -31,15 +31,17 @@ void Scene::updateLights() const {
   for (std::size_t lightIndex = 0; lightIndex < m_lights.size(); ++lightIndex) {
     const std::string locationBase = "uniLights[" + std::to_string(lightIndex) + "].";
 
-    const std::string posLocation   = locationBase + "position";
-    const std::string dirLocation   = locationBase + "direction";
-    const std::string colorLocation = locationBase + "color";
-    const std::string angleLocation = locationBase + "angle";
+    const std::string posLocation    = locationBase + "position";
+    const std::string dirLocation    = locationBase + "direction";
+    const std::string colorLocation  = locationBase + "color";
+    const std::string energyLocation = locationBase + "energy";
+    const std::string angleLocation  = locationBase + "angle";
 
-    m_program.sendUniform(posLocation, m_lights[lightIndex]->getHomogeneousPosition());
-    m_program.sendUniform(dirLocation, m_lights[lightIndex]->getDirection());
-    m_program.sendUniform(colorLocation, m_lights[lightIndex]->getColor());
-    m_program.sendUniform(angleLocation, m_lights[lightIndex]->getAngle());
+    m_program.sendUniform(posLocation,    m_lights[lightIndex]->getHomogeneousPosition());
+    m_program.sendUniform(dirLocation,    m_lights[lightIndex]->getDirection());
+    m_program.sendUniform(colorLocation,  m_lights[lightIndex]->getColor());
+    m_program.sendUniform(energyLocation, m_lights[lightIndex]->getEnergy());
+    m_program.sendUniform(angleLocation,  m_lights[lightIndex]->getAngle());
   }
 }
 
