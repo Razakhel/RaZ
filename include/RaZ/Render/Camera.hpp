017df0dbfc0aa6c5c5ced79301de78663a17ebe3
Razakhel <romain.milbert@gmail.com>
2018-05-25 13:52:22 +0200

    [Update] Added a view matrix computation in Camera to handle free-flying [Update] Renamed Camera::lookAt() to computeLookAt() for consistency [Update] Additional cast in Camera's ctor to remove another warning [Update] Renaming from pi<float> to PI<float>, after previous change

--- include/RaZ/Render/Camera.hpp
+++ include/RaZ/Render/Camera.hpp
@@ -14,19 +14,18 @@ namespace Raz {
 
 class Camera : public Transform {
 public:
-  Camera(unsigned int frameWidth,
-         unsigned int frameHeight,
+  Camera(unsigned int frameWidth, unsigned int frameHeight,
          float fieldOfViewDegrees,
-         float nearPlane,
-         float farPlane,
+         float nearPlane, float farPlane,
          const Vec3f& position = Vec3f(0.f))
-    : m_frameRatio{ static_cast<float>(frameWidth) / frameHeight },
-      m_fieldOfView{ fieldOfViewDegrees * pi<float> / 180 },
+    : m_frameRatio{ static_cast<float>(frameWidth) / static_cast<float>(frameHeight) },
+      m_fieldOfView{ fieldOfViewDegrees * PI<float> / 180 },
       m_nearPlane{ nearPlane },
       m_farPlane{ farPlane } { m_position = position; }
 
+  Mat4f computeViewMatrix() const { return m_rotation.inverse() * computeTranslationMatrix(true); }
+  Mat4f computeLookAt(const Vec3f& target = Vec3f(0.f), const Vec3f& orientation = Vec3f({ 0.f, 1.f, 0.f })) const;
   Mat4f computePerspectiveMatrix() const;
-  Mat4f lookAt(const Vec3f& target = Vec3f(0.f), const Vec3f& orientation = Vec3f({ 0.f, 1.f, 0.f })) const;
 
 private:
   float m_frameRatio;
--- src/RaZ/Render/Camera.cpp
+++ src/RaZ/Render/Camera.cpp
@@ -2,6 +2,19 @@
 
 namespace Raz {
 
+Mat4f Camera::computeLookAt(const Vec3f& target, const Vec3f& orientation) const {
+  const Vec3f zAxis((m_position - target).normalize());
+  const Vec3f xAxis(zAxis.cross(orientation).normalize());
+  const Vec3f yAxis(xAxis.cross(zAxis));
+
+  const Mat4f res({{               xAxis[0],               yAxis[0],             -zAxis[0], 0.f },
+                   {               xAxis[1],               yAxis[1],             -zAxis[1], 0.f },
+                   {               xAxis[2],               yAxis[2],             -zAxis[2], 0.f },
+                   { xAxis.dot(-m_position), yAxis.dot(-m_position), zAxis.dot(m_position), 1.f }});
+
+  return res;
+}
+
 Mat4f Camera::computePerspectiveMatrix() const {
   const float halfFovTangent = std::tan(m_fieldOfView / 2.f);
   const float planeDist      = m_farPlane - m_nearPlane;
@@ -14,19 +27,6 @@ Mat4f Camera::computePerspectiveMatrix() const {
                           {          0.f,                0.f, -planeMult / planeDist, 1.f }});
 
   return projMatrix;
-}
-
-Mat4f Camera::lookAt(const Vec3f& target, const Vec3f& orientation) const {
-  const Vec3f zAxis((m_position - target).normalize());
-  const Vec3f xAxis(zAxis.cross(orientation).normalize());
-  const Vec3f yAxis(xAxis.cross(zAxis));
-
-  const Mat4f res({{               xAxis[0],               yAxis[0],             -zAxis[0], 0.f },
-                   {               xAxis[1],               yAxis[1],             -zAxis[1], 0.f },
-                   {               xAxis[2],               yAxis[2],             -zAxis[2], 0.f },
-                   { xAxis.dot(-m_position), yAxis.dot(-m_position), zAxis.dot(m_position), 1.f }});
-
-  return res;
 }
 
 } // namespace Raz
