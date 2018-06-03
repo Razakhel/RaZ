5fa512e1cf3961cf42c77f03cd6709546dffadd5
Razakhel <romain.milbert@gmail.com>
2018-06-03 22:57:42 +0200

    [Update] Main example uses the new Application class to hold everything [Update] Deleted many uniforms calls, now unneeded (done in Scene & App)

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,13 +4,13 @@
 #include "RaZ/RaZ.hpp"
 
 int main() {
-  Raz::Window window(800, 600, "RaZ", 4);
-  //window.disableVerticalSync();
+  Raz::WindowPtr window = std::make_unique<Raz::Window>(800, 600, "RaZ", 4);
+  //window->disableVerticalSync();
 
   Raz::VertexShaderPtr vertShader   = std::make_unique<Raz::VertexShader>("../shaders/vert.glsl");
   Raz::FragmentShaderPtr fragShader = std::make_unique<Raz::FragmentShader>("../shaders/cook-torrance.glsl");
 
-  Raz::Scene scene(std::move(vertShader), std::move(fragShader));
+  Raz::ScenePtr scene = std::make_unique<Raz::Scene>(std::move(vertShader), std::move(fragShader));
 
   Raz::CubemapPtr cubemap = std::make_unique<Raz::Cubemap>("../assets/skyboxes/lake_right.png", "../assets/skyboxes/lake_left.png",
                                                            "../assets/skyboxes/lake_top.png", "../assets/skyboxes/lake_bottom.png",
@@ -35,101 +35,90 @@ int main() {
                                                                 1.f,                            // Energy
                                                                 Raz::Vec3f({ 1.f, 1.f, 1.f })); // Color (R/G/B)
 
-  Raz::CameraPtr camera = std::make_unique<Raz::Camera>(window.getWidth(),
-                                                        window.getHeight(),
+  Raz::CameraPtr camera = std::make_unique<Raz::Camera>(window->getWidth(),
+                                                        window->getHeight(),
                                                         45.f,                            // Field of view
                                                         0.1f, 100.f,                     // Near plane, far plane
                                                         Raz::Vec3f({ 0.f, 0.f, -5.f })); // Initial position
 
-  const auto cubemapPtr = cubemap.get();
-  const auto modelPtr   = model.get();
-  const auto lightPtr   = light.get();
-  const auto cameraPtr  = camera.get();
+  const auto modelPtr  = model.get();
+  const auto lightPtr  = light.get();
+  const auto cameraPtr = camera.get();
+
+  scene->setCubemap(std::move(cubemap));
+  scene->addModel(std::move(model));
+  scene->addLight(std::move(light));
+  scene->updateLights();
+  scene->load();
+
+  Raz::Application app(std::move(window), std::move(scene), std::move(camera));
+
+  auto& windowPtr = app.getWindow();
+  auto& scenePtr  = app.getScene();
 
   // Allow wireframe toggling
   bool isWireframe = false;
-  window.addKeyCallback(Raz::Keyboard::Z, [&isWireframe] () {
+  windowPtr->addKeyCallback(Raz::Keyboard::Z, [&isWireframe] () {
     glPolygonMode(GL_FRONT_AND_BACK, ((isWireframe = !isWireframe) ? GL_LINE : GL_FILL));
   });
 
   // Camera controls
-  window.addKeyCallback(Raz::Keyboard::SPACE, [&cameraPtr] () { cameraPtr->move( 0.f,  0.5f, 0.f); });
-  window.addKeyCallback(Raz::Keyboard::V,     [&cameraPtr] () { cameraPtr->move( 0.f, -0.5f, 0.f); });
-  window.addKeyCallback(Raz::Keyboard::W,     [&cameraPtr] () { cameraPtr->move( 0.f,  0.f,  0.5f); });
-  window.addKeyCallback(Raz::Keyboard::S,     [&cameraPtr] () { cameraPtr->move( 0.f,  0.f, -0.5f); });
-  window.addKeyCallback(Raz::Keyboard::A,     [&cameraPtr] () { cameraPtr->move(-0.5f, 0.f,  0.f); });
-  window.addKeyCallback(Raz::Keyboard::D,     [&cameraPtr] () { cameraPtr->move( 0.5f, 0.f,  0.f); });
-  window.addKeyCallback(Raz::Keyboard::NUM8,  [&cameraPtr] () { cameraPtr->rotate(-10.f, 1.f, 0.f, 0.f); }); // Looking up
-  window.addKeyCallback(Raz::Keyboard::NUM2,  [&cameraPtr] () { cameraPtr->rotate( 10.f, 1.f, 0.f, 0.f); }); // Looking down
-  window.addKeyCallback(Raz::Keyboard::NUM4,  [&cameraPtr] () { cameraPtr->rotate(-10.f, 0.f, 1.f, 0.f); }); // Looking left
-  window.addKeyCallback(Raz::Keyboard::NUM6,  [&cameraPtr] () { cameraPtr->rotate( 10.f, 0.f, 1.f, 0.f); }); // Looking right
+  windowPtr->addKeyCallback(Raz::Keyboard::SPACE, [&cameraPtr] () { cameraPtr->move( 0.f,  0.5f, 0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::V,     [&cameraPtr] () { cameraPtr->move( 0.f, -0.5f, 0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::W,     [&cameraPtr] () { cameraPtr->move( 0.f,  0.f,  0.5f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::S,     [&cameraPtr] () { cameraPtr->move( 0.f,  0.f, -0.5f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::A,     [&cameraPtr] () { cameraPtr->move(-0.5f, 0.f,  0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::D,     [&cameraPtr] () { cameraPtr->move( 0.5f, 0.f,  0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::NUM8,  [&cameraPtr] () { cameraPtr->rotate(-10.f, 1.f, 0.f, 0.f); }); // Looking up
+  windowPtr->addKeyCallback(Raz::Keyboard::NUM2,  [&cameraPtr] () { cameraPtr->rotate( 10.f, 1.f, 0.f, 0.f); }); // Looking down
+  windowPtr->addKeyCallback(Raz::Keyboard::NUM4,  [&cameraPtr] () { cameraPtr->rotate(-10.f, 0.f, 1.f, 0.f); }); // Looking left
+  windowPtr->addKeyCallback(Raz::Keyboard::NUM6,  [&cameraPtr] () { cameraPtr->rotate( 10.f, 0.f, 1.f, 0.f); }); // Looking right
 
   // Mesh controls
-  window.addKeyCallback(Raz::Keyboard::T,     [&modelPtr] () { modelPtr->move( 0.f,  0.f,  0.5f); });
-  window.addKeyCallback(Raz::Keyboard::G,     [&modelPtr] () { modelPtr->move( 0.f,  0.f, -0.5f); });
-  window.addKeyCallback(Raz::Keyboard::F,     [&modelPtr] () { modelPtr->move(-0.5f, 0.f,  0.f); });
-  window.addKeyCallback(Raz::Keyboard::H,     [&modelPtr] () { modelPtr->move( 0.5f, 0.f,  0.f); });
-  window.addKeyCallback(Raz::Keyboard::X,     [&modelPtr] () { modelPtr->scale(0.5f); });
-  window.addKeyCallback(Raz::Keyboard::C,     [&modelPtr] () { modelPtr->scale(2.f); });
-  window.addKeyCallback(Raz::Keyboard::UP,    [&modelPtr] () { modelPtr->rotate(-10.f, 1.f, 0.f, 0.f); });
-  window.addKeyCallback(Raz::Keyboard::DOWN,  [&modelPtr] () { modelPtr->rotate( 10.f, 1.f, 0.f, 0.f); });
-  window.addKeyCallback(Raz::Keyboard::LEFT,  [&modelPtr] () { modelPtr->rotate(-10.f, 0.f, 1.f, 0.f); });
-  window.addKeyCallback(Raz::Keyboard::RIGHT, [&modelPtr] () { modelPtr->rotate( 10.f, 0.f, 1.f, 0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::T,     [&modelPtr] () { modelPtr->move( 0.f,  0.f,  0.5f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::G,     [&modelPtr] () { modelPtr->move( 0.f,  0.f, -0.5f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::F,     [&modelPtr] () { modelPtr->move(-0.5f, 0.f,  0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::H,     [&modelPtr] () { modelPtr->move( 0.5f, 0.f,  0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::X,     [&modelPtr] () { modelPtr->scale(0.5f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::C,     [&modelPtr] () { modelPtr->scale(2.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::UP,    [&modelPtr] () { modelPtr->rotate(-10.f, 1.f, 0.f, 0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::DOWN,  [&modelPtr] () { modelPtr->rotate( 10.f, 1.f, 0.f, 0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::LEFT,  [&modelPtr] () { modelPtr->rotate(-10.f, 0.f, 1.f, 0.f); });
+  windowPtr->addKeyCallback(Raz::Keyboard::RIGHT, [&modelPtr] () { modelPtr->rotate( 10.f, 0.f, 1.f, 0.f); });
 
   // Light controls
-  window.addKeyCallback(Raz::Keyboard::I, [&lightPtr, &scene] () { lightPtr->translate( 0.f,  0.f,  0.5f); scene.updateLights(); });
-  window.addKeyCallback(Raz::Keyboard::K, [&lightPtr, &scene] () { lightPtr->translate( 0.f,  0.f, -0.5f); scene.updateLights(); });
-  window.addKeyCallback(Raz::Keyboard::J, [&lightPtr, &scene] () { lightPtr->translate(-0.5f, 0.f,  0.f);  scene.updateLights(); });
-  window.addKeyCallback(Raz::Keyboard::L, [&lightPtr, &scene] () { lightPtr->translate( 0.5f, 0.f,  0.f);  scene.updateLights(); });
-
-  window.addKeyCallback(Raz::Keyboard::F5, [&scene] () {
-    scene.getProgram().updateShaders();
-    scene.load();
-    scene.updateLights();
+  windowPtr->addKeyCallback(Raz::Keyboard::I, [&lightPtr, &scenePtr] () {
+    lightPtr->translate(0.f, 0.f, 0.5f);
+    scenePtr->updateLights();
+  });
+  windowPtr->addKeyCallback(Raz::Keyboard::K, [&lightPtr, &scenePtr] () {
+    lightPtr->translate(0.f, 0.f, -0.5f);
+    scenePtr->updateLights();
+  });
+  windowPtr->addKeyCallback(Raz::Keyboard::J, [&lightPtr, &scenePtr] () {
+    lightPtr->translate(-0.5f, 0.f, 0.f);
+    scenePtr->updateLights();
+  });
+  windowPtr->addKeyCallback(Raz::Keyboard::L, [&lightPtr, &scenePtr] () {
+    lightPtr->translate(0.5f, 0.f, 0.f);
+    scenePtr->updateLights();
   });
 
-  scene.setCubemap(std::move(cubemap));
-  scene.addModel(std::move(model));
-  scene.addLight(std::move(light));
-  scene.updateLights();
-  //scene.setCamera(std::move(camera));
-  scene.load();
-
-  const Raz::Mat4f projectionMat = cameraPtr->computePerspectiveMatrix();
-
-  cubemapPtr->getProgram().use();
-  cubemapPtr->getProgram().sendUniform("uniProjectionMatrix", projectionMat);
-
-  const auto uniCameraPosLocation = scene.getProgram().recoverUniformLocation("uniCameraPos");
-  const auto uniViewProjLocation  = scene.getProgram().recoverUniformLocation("uniViewProjMatrix");
-
-  const auto uniCubemapViewLocation = cubemapPtr->getProgram().recoverUniformLocation("uniViewMatrix");
+  windowPtr->addKeyCallback(Raz::Keyboard::F5, [&app] () { app.updateShaders(); });
 
   auto lastTime = std::chrono::system_clock::now();
   uint16_t nbFrames = 0;
 
-  while (window.run()) {
+  while (app.run()) {
     const auto currentTime = std::chrono::system_clock::now();
     ++nbFrames;
 
     if (std::chrono::duration_cast<std::chrono::duration<float>>(currentTime - lastTime).count() >= 1.f) {
-      window.setTitle("RaZ - " + std::to_string(nbFrames) + " FPS");
+      app.getWindow()->setTitle("RaZ - " + std::to_string(nbFrames) + " FPS");
 
       nbFrames = 0;
       lastTime = currentTime;
     }
-
-    const Raz::Mat4f viewMat     = cameraPtr->computeViewMatrix();
-    const Raz::Mat4f viewProjMat = projectionMat * viewMat;
-
-    scene.getProgram().use();
-    scene.getProgram().sendUniform(uniCameraPosLocation, camera->getPosition());
-    scene.getProgram().sendUniform(uniViewProjLocation, viewProjMat);
-
-    cubemapPtr->getProgram().use();
-    cubemapPtr->getProgram().sendUniform(uniCubemapViewLocation, Raz::Mat4f(Raz::Mat3f(viewMat)));
-
-    scene.render(viewProjMat);
   }
 
   return EXIT_SUCCESS;
