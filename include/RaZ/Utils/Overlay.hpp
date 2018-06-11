11810526d079ea64b12cab6b6147dab43af720bc
Razakhel <romain.milbert@gmail.com>
2018-06-11 19:59:37 +0200

    [Update] Checkbox now has two different actions available when ON/OFF [Update] Added frame time & FPS counter as overlay elements [Update] Added functions to ease overlay elements' creations

--- include/RaZ/Utils/Overlay.hpp
+++ include/RaZ/Utils/Overlay.hpp
@@ -8,19 +8,29 @@
 #include <unordered_map>
 #include <vector>
 
+#include "glfw/include/GLFW/glfw3.h"
+
 namespace Raz {
 
 enum class OverlayElementType { TEXT,
                                 BUTTON,
-                                CHECKBOX };
+                                CHECKBOX,
+                                FRAME_TIME,
+                                FPS_COUNTER };
 
-using OverlayElements = std::vector<std::tuple<OverlayElementType, std::string, std::function<void()>>>;
+using OverlayElements = std::vector<std::tuple<OverlayElementType, std::string, std::function<void()>, std::function<void()>>>;
 
 class Overlay {
 public:
   explicit Overlay(GLFWwindow* window);
 
-  void addElement(OverlayElementType type, const std::string& name, std::function<void()> = nullptr);
+  void addElement(OverlayElementType type, const std::string& text,
+                  std::function<void()> actionOn = nullptr, std::function<void()> actionOff = nullptr);
+  void addText(const std::string& text);
+  void addButton(const std::string& text, std::function<void()> action);
+  void addCheckbox(const std::string& text, bool initVal, std::function<void()> actionOn, std::function<void()> actionOff);
+  void addFrameTime(const std::string& formattedText);
+  void addFpsCounter(const std::string& formattedText);
   void render();
 
   ~Overlay();
--- src/RaZ/Utils/Overlay.cpp
+++ src/RaZ/Utils/Overlay.cpp
@@ -11,11 +11,33 @@ Overlay::Overlay(GLFWwindow* window) {
   ImGui::StyleColorsDark();
 }
 
-void Overlay::addElement(OverlayElementType type, const std::string& name, std::function<void()> action) {
+void Overlay::addElement(OverlayElementType type, const std::string& text,
+                         std::function<void()> actionOn, std::function<void()> actionOff) {
   if (type == OverlayElementType::CHECKBOX)
     m_toggles.emplace(m_elements.size(), false);
 
-  m_elements.emplace_back(std::make_tuple(type, name, action));
+  m_elements.emplace_back(std::make_tuple(type, text, std::move(actionOn), std::move(actionOff)));
+}
+
+void Overlay::addText(const std::string& text) {
+  addElement(OverlayElementType::TEXT, text);
+}
+
+void Overlay::addButton(const std::string& text, std::function<void()> action) {
+  addElement(OverlayElementType::BUTTON, text, std::move(action));
+}
+
+void Overlay::addCheckbox(const std::string& text, bool initVal, std::function<void()> actionOn, std::function<void()> actionOff) {
+  addElement(OverlayElementType::CHECKBOX, text, std::move(actionOn), std::move(actionOff));
+  m_toggles[m_elements.size() - 1] = initVal;
+}
+
+void Overlay::addFrameTime(const std::string& formattedText) {
+  addElement(OverlayElementType::FRAME_TIME, formattedText);
+}
+
+void Overlay::addFpsCounter(const std::string& formattedText) {
+  addElement(OverlayElementType::FPS_COUNTER, formattedText);
 }
 
 void Overlay::render() {
@@ -26,7 +48,7 @@ void Overlay::render() {
 
     switch (std::get<0>(element)) {
       case OverlayElementType::TEXT:
-        ImGui::Text(std::get<1>(element).c_str());
+        ImGui::TextUnformatted(std::get<1>(element).c_str());
         break;
 
       case OverlayElementType::BUTTON:
@@ -34,11 +56,25 @@ void Overlay::render() {
           std::get<2>(element)();
         break;
 
-      case OverlayElementType::CHECKBOX:
+      case OverlayElementType::CHECKBOX: {
+        bool prevValue = m_toggles[eltIndex];
         ImGui::Checkbox(std::get<1>(element).c_str(), &m_toggles[eltIndex]);
 
-        if (m_toggles[eltIndex])
-          std::get<2>(element)();
+        if (m_toggles[eltIndex] != prevValue) {
+          if (m_toggles[eltIndex])
+            std::get<2>(element)();
+          else
+            std::get<3>(element)();
+        }
+        break;
+      }
+
+      case OverlayElementType::FRAME_TIME:
+        ImGui::Text(std::get<1>(element).c_str(), 1000.f / ImGui::GetIO().Framerate);
+        break;
+
+      case OverlayElementType::FPS_COUNTER:
+        ImGui::Text(std::get<1>(element).c_str(), ImGui::GetIO().Framerate);
         break;
     }
   }
