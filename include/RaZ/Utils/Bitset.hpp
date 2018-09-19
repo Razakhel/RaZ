ed9cfb30a59e27c0a9ef7d016e46a3b5750ae817
Razakhel <romain.milbert@gmail.com>
2018-09-19 16:57:53 +0200

    [Add] Created a basic dynamic Bitset class, handling bitset operations

---
+++ include/RaZ/Utils/Bitset.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#ifndef RAZ_BITSET_HPP
+#define RAZ_BITSET_HPP
+
+#include <vector>
+
+namespace Raz {
+
+class Bitset {
+public:
+  Bitset() = default;
+  explicit Bitset(std::size_t bitCount) : m_bits(bitCount) {}
+
+  const std::vector<bool>& getBits() const { return m_bits; }
+  std::vector<bool>& getBits() { return m_bits; }
+  std::size_t getSize() const { return m_bits.size(); }
+
+  void setBit(std::size_t position, bool value = true);
+  void resize(std::size_t newSize) { m_bits.resize(newSize); }
+
+  Bitset operator&(const Bitset& bitset) const;
+  Bitset operator|(const Bitset& bitset) const;
+  Bitset& operator|=(const Bitset& bitset);
+  Bitset& operator&=(const Bitset& bitset);
+  bool operator[](std::size_t index) const { return m_bits[index]; }
+  bool operator==(const Bitset& bitset) const { return std::equal(m_bits.cbegin(), m_bits.cend(), bitset.getBits().cbegin()); }
+  bool operator!=(const Bitset& bitset) const { return !(*this == bitset); }
+
+private:
+  std::vector<bool> m_bits {};
+};
+
+} // namespace Raz
+
+#endif // RAZ_BITSET_HPP
+
---
+++ src/RaZ/Utils/Bitset.cpp
@@ -0,0 +1,36 @@
+#include "RaZ/Utils/Bitset.hpp"
+
+namespace Raz {
+
+void Bitset::setBit(std::size_t position, bool value) {
+  if (position >= m_bits.size())
+    m_bits.resize(position + 1);
+
+  m_bits[position] = value;
+}
+
+Bitset Bitset::operator&(const Bitset& bitset) const {
+  Bitset res = *this;
+  res &= bitset;
+  return res;
+}
+
+Bitset Bitset::operator|(const Bitset& bitset) const {
+  Bitset res = *this;
+  res |= bitset;
+  return res;
+}
+
+Bitset& Bitset::operator&=(const Bitset& bitset) {
+  for (std::size_t bitIndex = 0; bitIndex < std::min(m_bits.size(), bitset.getSize()); ++bitIndex)
+    m_bits[bitIndex] = m_bits[bitIndex] & bitset.getBits()[bitIndex];
+  return *this;
+}
+
+Bitset& Bitset::operator|=(const Bitset& bitset) {
+  for (std::size_t bitIndex = 0; bitIndex < std::min(m_bits.size(), bitset.getSize()); ++bitIndex)
+    m_bits[bitIndex] = m_bits[bitIndex] | bitset.getBits()[bitIndex];
+  return *this;
+}
+
+} // namespace Raz
