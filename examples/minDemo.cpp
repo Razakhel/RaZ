3014ac3ddd4cad1d7c4c622668f78470b54d34db
Razakhel <romain.milbert@gmail.com>
2018-09-28 00:17:10 +0200

    [Update] Modified minimal demo using the ECS, with PBR ball & dir light

--- examples/minDemo.cpp
+++ examples/minDemo.cpp
@@ -1,17 +1,23 @@
 #include "RaZ/RaZ.hpp"
 
 int main() {
-  Raz::WindowPtr window = Raz::Window::create(800, 600);
+  Raz::Application app;
+  Raz::World& world = app.addWorld(Raz::World(2));
 
-  const Raz::Application app(std::move(window),
-                             Raz::Scene::create(Raz::VertexShader::create("../../shaders/vert.glsl"),
-                                                Raz::FragmentShader::create("../../shaders/cook-torrance.glsl")));
+  auto& render = world.addSystem<Raz::RenderSystem>(1280, 720, "RaZ");
+  render.setProgram(Raz::ShaderProgram(Raz::VertexShader::create("../../shaders/vert.glsl"),
+                                       Raz::FragmentShader::create("../../shaders/cook-torrance.glsl")));
 
-  app.getScene()->addModel(Raz::Model::import("../../assets/meshes/ball.obj"));
-  app.getScene()->addLight(Raz::DirectionalLight::create(Raz::Vec3f({ 0.f, 0.f, 1.f })));
-  app.getScene()->load();
+  render.getCameraEntity().getComponent<Raz::Transform>().setPosition(Raz::Vec3f({0.f, 0.f, -5.f}));
 
-  app.getCamera()->translate(0.f, 0.f, -5.f);
+  auto& mesh = world.addEntityWithComponent<Raz::Transform>();
+  mesh.addComponent<Raz::Mesh>("../../assets/meshes/ball.obj");
+
+  auto& light = world.addEntityWithComponent<Raz::Transform>();
+  light.addComponent<Raz::Light>(Raz::LightType::DIRECTIONAL,   // Type
+                                 Raz::Vec3f({ 0.f, 0.f, 1.f }), // Direction
+                                 1.f,                           // Energy
+                                 Raz::Vec3f(1.f));              // Color (R/G/B)
 
   while (app.run());
 
