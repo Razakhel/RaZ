5e6907858a95d41df3b041308537011234272f15
Razakhel <romain.milbert@gmail.com>
2018-10-10 00:11:14 +0200

    [Add] Added a Component test file, checking IDs [Update] Tests' CMakeLists now handles root tests

---
+++ tests/RaZ/Component.cpp
@@ -0,0 +1,22 @@
+#include "catch/catch.hpp"
+#include "RaZ/Component.hpp"
+#include "RaZ/Math/Transform.hpp"
+#include "RaZ/Render/Camera.hpp"
+#include "RaZ/Render/Light.hpp"
+#include "RaZ/Render/Mesh.hpp"
+
+TEST_CASE("Components IDs checks") {
+  // With the CRTP, every component gets a different constant ID with the first call
+  // The ID is incremented with every distinct component call
+  // No matter how many times one component is checked, it will always have the same ID
+
+  REQUIRE(Raz::Component::getId<Raz::Mesh>() == 0);      // Raz::Mesh's ID is fetched, gets the ID 0; ID incremented
+  REQUIRE(Raz::Component::getId<Raz::Light>() == 1);     // Raz::Light's ID is fetched, get the ID 1; ID incremented
+  REQUIRE(Raz::Component::getId<Raz::Transform>() == 2); // Raz::Transform's ID is fetched, get the ID 2; ID incremented
+  REQUIRE(Raz::Component::getId<Raz::Camera>() == 3);    // Raz::Camera's ID is fetched, get the ID 3; ID incremented
+
+  REQUIRE(Raz::Component::getId<Raz::Light>() == 1);     // Raz::Light already has the ID 1
+  REQUIRE(Raz::Component::getId<Raz::Camera>() == 3);    // Raz::Camera already has the ID 3
+  REQUIRE(Raz::Component::getId<Raz::Mesh>() == 0);      // Raz::Mesh already has the ID 0
+  REQUIRE(Raz::Component::getId<Raz::Transform>() == 2); // Raz::Transform already has the ID 2
+}
