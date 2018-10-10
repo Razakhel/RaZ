bfd9968caa7f44665f2dda0d43629b5166395984
Razakhel <romain.milbert@gmail.com>
2018-10-11 00:18:52 +0200

    [Update] Added Entity bitset checks

--- tests/RaZ/Entity.cpp
+++ tests/RaZ/Entity.cpp
@@ -1,6 +1,7 @@
 #include "catch/catch.hpp"
 #include "RaZ/Entity.hpp"
 #include "RaZ/Math/Transform.hpp"
+#include "RaZ/Render/Light.hpp"
 #include "RaZ/Render/Mesh.hpp"
 
 namespace {
@@ -42,4 +43,30 @@ TEST_CASE("Entity-component manipulations") {
   entity0.removeComponent<Raz::Transform>();
   REQUIRE_FALSE(entity0.hasComponent<Raz::Transform>());
   REQUIRE_THROWS(entity0.getComponent<Raz::Transform>());
+}
+
+TEST_CASE("Entity bitset checks") {
+  REQUIRE(entity1.getEnabledComponents().isEmpty());
+  entity1.addComponent<Raz::Transform>();
+  REQUIRE_FALSE(entity1.getEnabledComponents().isEmpty());
+
+  REQUIRE(entity1.getEnabledComponents().getEnabledBitCount() == 1);
+  REQUIRE(entity1.getEnabledComponents()[Raz::Component::getId<Raz::Transform>()]);
+  REQUIRE(entity1.getEnabledComponents().getSize() == Raz::Component::getId<Raz::Transform>() + 1);
+
+  REQUIRE(entity2.getEnabledComponents().isEmpty());
+  entity2.addComponent<Raz::Light>(Raz::LightType::POINT, 10.f);
+  REQUIRE_FALSE(entity2.getEnabledComponents().isEmpty());
+
+  REQUIRE(entity2.getEnabledComponents().getEnabledBitCount() == 1);
+  REQUIRE(entity2.getEnabledComponents()[Raz::Component::getId<Raz::Light>()]);
+  REQUIRE(entity2.getEnabledComponents().getSize() == Raz::Component::getId<Raz::Light>() + 1);
+
+  //  Match test
+  //     ---
+  //    0 0 1     -> Transform
+  // &  0 1 0     -> Light
+  //   _______
+  // =  0 0 0
+  REQUIRE((entity1.getEnabledComponents() & entity2.getEnabledComponents()) == Raz::Bitset(entity1.getEnabledComponents().getSize()));
 }
