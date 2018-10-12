bc8efaeaeb031c477cff5312676ebcbfc7fd4a9e
Razakhel <romain.milbert@gmail.com>
2018-10-12 10:28:54 +0200

    [Update] Externalized float near-equality check in separate file

---
+++ include/RaZ/Utils/FloatUtils.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef RAZ_FLOATUTILS_HPP
+#define RAZ_FLOATUTILS_HPP
+
+#include <cmath>
+#include <limits>
+
+namespace Raz {
+
+namespace FloatUtils {
+
+// Near-equality floating point check
+template <typename T>
+inline bool checkNearEquality(T val1, T val2) {
+  static_assert(std::is_floating_point<T>::value, "Error: Values' type must be floating point.");
+
+  // Using absolute & relative tolerances for floating points types: http://www.realtimecollisiondetection.net/pubs/Tolerances/
+  // Could be a better idea to use ULPs checking. May be slower though?
+  const T absDiff = std::abs(val1 - val2);
+  return (absDiff <= std::numeric_limits<T>::epsilon() * std::max({ static_cast<T>(1), std::abs(val1), std::abs(val2) }));
+}
+
+} // namespace FloatUtils
+
+} // namespace Raz
+
+#endif // RAZ_FLOATUTILS_HPP
