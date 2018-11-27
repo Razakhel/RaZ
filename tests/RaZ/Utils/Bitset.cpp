e0f8c019ba5564abc65991097496c37b166e9721
Razakhel <romain.milbert@gmail.com>
2018-11-27 18:25:03 +0100

    [Update] Added a unit test for Bitset's bitshift operators

--- tests/RaZ/Utils/Bitset.cpp
+++ tests/RaZ/Utils/Bitset.cpp
@@ -73,3 +73,16 @@ TEST_CASE("Bitset manipulations") {
   REQUIRE(~alternated1 == alternated2);
   REQUIRE(~alternated2 == alternated1);
 }
+
+TEST_CASE("Bitset shifts") {
+  REQUIRE((alternated1 << 1) == Raz::Bitset({ true, false, true, false, true, false, false })); // 1 0 1 0 1 0 0
+  REQUIRE((alternated1 >> 1) == Raz::Bitset({ true, false, true, false, true })); // 1 0 1 0 1
+
+  REQUIRE((alternated1 >> alternated1.getSize()).getSize() == 0);
+
+  Raz::Bitset shiftTest = alternated1;
+  shiftTest >>= 1; // 1 0 1 0 1 0 0
+  shiftTest <<= 1; // 1 0 1 0 1 0
+
+  REQUIRE(shiftTest == alternated1);
+}
