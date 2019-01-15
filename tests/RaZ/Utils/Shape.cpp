fe54b5338383eba5ab787f44523fd997d9f0e36d
Razakhel <romain.milbert@gmail.com>
2019-01-16 00:07:14 +0100

    [Tests/Utils/Shape] Added basic tests for Plane
    - Asserting that the 3 Plane constructors build the same plane

--- tests/RaZ/Utils/Shape.cpp
+++ tests/RaZ/Utils/Shape.cpp
@@ -7,6 +7,8 @@ namespace {
 const Raz::Line line1(Raz::Vec3f({ 0.f, 0.f, 0.f }), Raz::Vec3f({ 1.f, 0.f, 0.f }));
 const Raz::Line line2(Raz::Vec3f(-1.f), Raz::Vec3f(1.f));
 
+const Raz::Plane plane(1.f, Raz::Axis::Y);
+
 } // namespace
 
 TEST_CASE("Line basic checks") {
@@ -15,4 +17,16 @@ TEST_CASE("Line basic checks") {
 
   REQUIRE(Raz::FloatUtils::checkNearEquality(line2.computeLength(), 3.464101615f));
   REQUIRE(Raz::FloatUtils::checkNearEquality(line2.computeSquaredLength(), 12.f));
+}
+
+TEST_CASE("Plane basic checks") {
+  const Raz::Plane plane2(Raz::Vec3f({ 0.f, 1.f, 0.f }), Raz::Axis::Y);
+  const Raz::Plane plane3(Raz::Vec3f({ 1.f, 1.f, 0.f }), Raz::Vec3f({ -1.f, 1.f, -1.f }), Raz::Vec3f({ 0.f, 1.f, 1.f }));
+
+  // Checking that the 3 planes are strictly equal to each other
+  REQUIRE(Raz::FloatUtils::checkNearEquality(plane.getDistance(), plane2.getDistance()));
+  REQUIRE(Raz::FloatUtils::checkNearEquality(plane.getDistance(), plane3.getDistance()));
+
+  REQUIRE(plane.getNormal() == plane2.getNormal());
+  REQUIRE(plane.getNormal() == plane3.getNormal());
 }
