e2425d79193d8287351304584ebd42c1250773d2
Razakhel <romain.milbert@gmail.com>
2019-03-15 21:58:41 +0100

    [Component] Added documentation for the Component class

--- include/RaZ/Component.hpp
+++ include/RaZ/Component.hpp
@@ -10,8 +10,15 @@ namespace Raz {
 class Component;
 using ComponentPtr = std::unique_ptr<Component>;
 
+/// Component class representing a base Component to be inherited.
 class Component {
 public:
+  /// Gets the ID of the given component.
+  /// It uses CRTP to assign a different ID to each component it is called with.
+  /// This function will be compiled every time it is called with a different component type, incrementing the assigned index.
+  /// Note that it must be called directly from Component, and a derived class must be given (Component::getId<DerivedComponent>()).
+  /// \tparam T Type of the component to get the ID for.
+  /// \return Given component's ID.
   template <typename T> static std::size_t getId();
 
   virtual ~Component() = default;
