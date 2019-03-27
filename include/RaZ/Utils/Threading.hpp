cb06e0bfbf1b33e2ad11a4bebe8cc115fc1acc2c
Razakhel <romain.milbert@gmail.com>
2019-03-27 18:12:06 +0100

    [Utils/Threading] Added Threading::sleep() to pause the current thread

--- include/RaZ/Utils/Threading.hpp
+++ include/RaZ/Utils/Threading.hpp
@@ -11,6 +11,7 @@
 #ifdef RAZ_THREADS_AVAILABLE
 
 #include <future>
+#include <thread>
 
 namespace Raz {
 
@@ -20,6 +21,10 @@ namespace Threading {
 /// This number doesn't necessarily represent the CPU's actual number of threads.
 /// \return Number of threads available.
 unsigned int getSystemThreadCount();
+
+/// Pauses the current thread for the specified amount of time.
+/// \param milliseconds Pause duration in milliseconds.
+inline void sleep(uint64_t milliseconds) { std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds)); }
 
 /// Calls a function asynchronously, to be executed without blocking the calling thread.
 /// \tparam T Return type of the given function.
