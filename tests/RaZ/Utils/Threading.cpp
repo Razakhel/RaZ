53a2f2dc2a2bb3a4ecfc63844ab3bd9a381ae42d
Razakhel <romain.milbert@gmail.com>
2019-04-01 20:59:30 +0200

    [Tests/Threading] Added parallelize() (with index range) unit tests
    - Testing with divisible and indivisible vectors sizes, to check that the index split is made correctly
    - Both tests parallelize on 4 threads

--- tests/RaZ/Utils/Threading.cpp
+++ tests/RaZ/Utils/Threading.cpp
@@ -1,7 +1,34 @@
+#include <numeric>
+#include <random>
+
 #include "catch/catch.hpp"
 #include "RaZ/Utils/Threading.hpp"
 
 #ifdef RAZ_THREADS_AVAILABLE
+
+namespace {
+
+void fillRandom(std::vector<int>& values) {
+  std::random_device randDevice;
+  std::mt19937 randGen(randDevice());
+  std::uniform_int_distribution<int> randDist(0, static_cast<int>(values.size()) - 1);
+
+  for (int& val : values)
+    val = randDist(randGen);
+}
+
+std::size_t computeSum(const std::vector<int>& values) {
+  return std::accumulate(values.cbegin(), values.cend(), static_cast<std::size_t>(0));
+}
+
+void parallelIncrementation(std::vector<int>& values) {
+  Raz::Threading::parallelize(values, [&values] (Raz::Threading::IndexRange range) {
+    for (std::size_t i = range.beginIndex; i < range.endIndex; ++i)
+      ++values[i];
+  }, 4);
+}
+
+} // namespace
 
 TEST_CASE("Async basic") {
   std::future<int> res = Raz::Threading::launchAsync([] () noexcept {
@@ -11,6 +38,28 @@ TEST_CASE("Async basic") {
   res.wait();
 
   REQUIRE(res.get() == 42);
+}
+
+TEST_CASE("Parallelize (with indices) divisible vector increment") {
+  std::vector<int> values(2048); // Choosing a size that can be easily divided
+  fillRandom(values);
+
+  const std::size_t sumBeforeIncrement = computeSum(values);
+  parallelIncrementation(values);
+  const std::size_t sumAfterIncrement = computeSum(values);
+
+  REQUIRE(sumBeforeIncrement + values.size() == sumAfterIncrement);
+}
+
+TEST_CASE("Parallelize (with indices) indivisible vector increment") {
+  std::vector<int> values(2083); // Choosing a prime number as size, so that it can't be easily divided
+  fillRandom(values);
+
+  const std::size_t sumBeforeIncrement = computeSum(values);
+  parallelIncrementation(values);
+  const std::size_t sumAfterIncrement = computeSum(values);
+
+  REQUIRE(sumBeforeIncrement + values.size() == sumAfterIncrement);
 }
 
 #endif // RAZ_THREADS_AVAILABLE
