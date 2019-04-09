c3be4ecf479629836e84e9944ffccb612a4a32d4
Razakhel <romain.milbert@gmail.com>
2019-04-09 17:36:23 +0200

    [Render/GraphicObjects] An EBO now holds indices to display lines
    - Declared move ctors/assignments operators as noexcept
    
    Signed-off-by: Razakhel <romain.milbert@gmail.com>

--- include/RaZ/Render/GraphicObjects.hpp
+++ include/RaZ/Render/GraphicObjects.hpp
@@ -27,30 +27,33 @@ class ElementBuffer {
 public:
   ElementBuffer() { glGenBuffers(1, &m_index); }
   ElementBuffer(const ElementBuffer&) = delete;
-  ElementBuffer(ElementBuffer&&) = default;
+  ElementBuffer(ElementBuffer&&) noexcept = default;
 
   GLuint getIndex() const { return m_index; }
-  const std::vector<unsigned int>& getIndices() const { return m_indices; }
-  std::vector<unsigned int>& getIndices() { return m_indices; }
+  const std::vector<unsigned int>& getLineIndices() const { return m_lineIndices; }
+  std::vector<unsigned int>& getLineIndices() { return m_lineIndices; }
+  const std::vector<unsigned int>& getTriangleIndices() const { return m_triangleIndices; }
+  std::vector<unsigned int>& getTriangleIndices() noexcept { return m_triangleIndices; }
 
   void bind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_index); }
   void unbind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); }
 
   ElementBuffer& operator=(const ElementBuffer&) = delete;
-  ElementBuffer& operator=(ElementBuffer&&) = default;
+  ElementBuffer& operator=(ElementBuffer&&) noexcept = default;
 
   ~ElementBuffer() { glDeleteBuffers(1, &m_index); }
 
 private:
   GLuint m_index {};
-  std::vector<unsigned int> m_indices {};
+  std::vector<unsigned int> m_lineIndices {};
+  std::vector<unsigned int> m_triangleIndices {};
 };
 
 class VertexBuffer {
 public:
   VertexBuffer() { glGenBuffers(1, &m_index); }
   VertexBuffer(const VertexBuffer&) = delete;
-  VertexBuffer(VertexBuffer&&) = default;
+  VertexBuffer(VertexBuffer&&) noexcept = default;
 
   GLuint getIndex() const { return m_index; }
   const std::vector<Vertex>& getVertices() const { return m_vertices; }
@@ -60,7 +63,7 @@ public:
   void unbind() const { glBindBuffer(GL_ARRAY_BUFFER, 0); }
 
   VertexBuffer& operator=(const VertexBuffer&) = delete;
-  VertexBuffer& operator=(VertexBuffer&&) = default;
+  VertexBuffer& operator=(VertexBuffer&&) noexcept = default;
 
   ~VertexBuffer() { glDeleteBuffers(1, &m_index); }
 
@@ -73,17 +76,17 @@ class VertexArray {
 public:
   VertexArray() { glGenVertexArrays(1, &m_index); }
   VertexArray(const VertexArray&) = delete;
-  VertexArray(VertexArray&&) = default;
+  VertexArray(VertexArray&&) noexcept = default;
 
   GLuint getIndex() const { return m_index; }
   const ElementBuffer& getEbo() const { return m_ebo; }
-  ElementBuffer& getEbo() { return m_ebo; }
+  ElementBuffer& getEbo() noexcept { return m_ebo; }
 
   void bind() const { glBindVertexArray(m_index); m_ebo.bind(); }
   void unbind() const { glBindVertexArray(0); m_ebo.unbind(); }
 
   VertexArray& operator=(const VertexArray&) = delete;
-  VertexArray& operator=(VertexArray&&) = default;
+  VertexArray& operator=(VertexArray&&) noexcept = default;
 
   ~VertexArray() { glDeleteVertexArrays(1, &m_index); }
 
