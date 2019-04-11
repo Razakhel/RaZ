b7f96666f4411a20db3f557a21c16003a4d4bd13
Razakhel <romain.milbert@gmail.com>
2019-04-11 22:55:25 +0200

    [Render/UniformBuffer] Specialized UniformBuffer's move ctor & operator=
    - The ctor passes the ID from the moved uniform buffer to the receiving one, and invalidates the former
    - The assignment operator swaps the IDs so that the receiving one is still usable & the moved one frees itself
    - This fixes issues where uniform buffers' indices would be instantly invalidated when one is deleted after a move
    
    - Started to remove GLEW usage directly in header (the .inl still remains)
    
    Signed-off-by: Razakhel <romain.milbert@gmail.com>

--- src/RaZ/Render/UniformBuffer.cpp
+++ src/RaZ/Render/UniformBuffer.cpp
@@ -2,6 +2,10 @@
 
 namespace Raz {
 
+UniformBuffer::UniformBuffer() {
+  glGenBuffers(1, &m_index);
+}
+
 UniformBuffer::UniformBuffer(unsigned int size, unsigned int bindingIndex) : UniformBuffer() {
   bind();
   glBufferData(GL_UNIFORM_BUFFER, size, nullptr, GL_STATIC_DRAW);
@@ -9,12 +13,40 @@ UniformBuffer::UniformBuffer(unsigned int size, unsigned int bindingIndex) : Uni
   unbind();
 }
 
+UniformBuffer::UniformBuffer(UniformBuffer&& ubo) noexcept
+  : m_index{ std::exchange(ubo.m_index, GL_INVALID_INDEX) } {}
+
 void UniformBuffer::bindUniformBlock(const ShaderProgram& program, unsigned int uboIndex, unsigned int bindingIndex) const {
   glUniformBlockBinding(program.getIndex(), uboIndex, bindingIndex);
 }
 
 void UniformBuffer::bindUniformBlock(const ShaderProgram& program, const std::string& uboName, unsigned int bindingIndex) const {
   bindUniformBlock(program, glGetUniformBlockIndex(program.getIndex(), uboName.c_str()), bindingIndex);
+}
+
+void UniformBuffer::bindBufferBase(unsigned int bindingIndex) const {
+  glBindBufferBase(GL_UNIFORM_BUFFER, bindingIndex, m_index);
+}
+
+void UniformBuffer::bind() const {
+  glBindBuffer(GL_UNIFORM_BUFFER, m_index);
+}
+
+void UniformBuffer::unbind() const {
+  glBindBuffer(GL_UNIFORM_BUFFER, 0);
+}
+
+UniformBuffer& UniformBuffer::operator=(UniformBuffer&& ubo) noexcept {
+  std::swap(m_index, ubo.m_index);
+
+  return *this;
+}
+
+UniformBuffer::~UniformBuffer() {
+  if (m_index == GL_INVALID_INDEX)
+    return;
+
+  glDeleteBuffers(1, &m_index);
 }
 
 } // namespace Raz
