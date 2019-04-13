4dfb7aec7f649ddac56772da86da7fe03a4c11f4
Razakhel <romain.milbert@gmail.com>
2019-04-13 10:21:41 +0200

    [Utils/Image] Added documentation for the Image & ImageData classes
    Signed-off-by: Razakhel <romain.milbert@gmail.com>

--- include/RaZ/Utils/Image.hpp
+++ include/RaZ/Utils/Image.hpp
@@ -26,16 +26,20 @@ enum class ImageDataType : uint8_t {
   FLOAT
 };
 
+/// ImageData class, representing data held by an Image.
 struct ImageData {
   virtual ImageDataType getDataType() const = 0;
   virtual const void* getDataPtr() const = 0;
   virtual void* getDataPtr() = 0;
 
+  /// Checks if the image doesn't contain data.
+  /// \return True if the image has no data, false otherwise.
   virtual bool isEmpty() const = 0;
 
   virtual ~ImageData() = default;
 };
 
+/// ImageData in bytes.
 struct ImageDataB : public ImageData {
   ImageDataType getDataType() const override { return ImageDataType::BYTE; }
   const void* getDataPtr() const override { return data.data(); }
@@ -44,11 +48,14 @@ struct ImageDataB : public ImageData {
   template <typename... Args>
   static ImageDataBPtr create(Args&&... args) { return std::make_unique<ImageDataB>(std::forward<Args>(args)...); }
 
+  /// Checks if the image doesn't contain data.
+  /// \return True if the image has no data, false otherwise.
   bool isEmpty() const override { return data.empty(); }
 
   std::vector<uint8_t> data;
 };
 
+/// ImageData in floating point values (for High Dynamic Range (HDR) images).
 struct ImageDataF : public ImageData {
   ImageDataType getDataType() const override { return ImageDataType::FLOAT; }
   const void* getDataPtr() const override { return data.data(); }
@@ -57,6 +64,8 @@ struct ImageDataF : public ImageData {
   template <typename... Args>
   static ImageDataFPtr create(Args&&... args) { return std::make_unique<ImageDataF>(std::forward<Args>(args)...); }
 
+  /// Checks if the image doesn't contain data.
+  /// \return True if the image has no data, false otherwise.
   bool isEmpty() const override { return data.empty(); }
 
   std::vector<float> data;
@@ -70,6 +79,7 @@ enum class ImageColorspace {
   DEPTH      = 6402   // GL_DEPTH_COMPONENT
 };
 
+/// Image class, handling images of different formats.
 class Image {
 public:
   Image() = default;
@@ -83,15 +93,35 @@ public:
 
   template <typename... Args> static ImagePtr create(Args&&... args) { return std::make_unique<Image>(std::forward<Args>(args)...); }
 
+  /// Checks if the image doesn't contain data.
+  /// \return True if the image has no data, false otherwise.
   bool isEmpty() const { return (!m_data || m_data->isEmpty()); }
+  /// Reads the image to memory.
+  /// \param filePath Path to the image to read.
+  /// \param reverse Reverse the image when reading.
   void read(const std::string& filePath, bool reverse = false);
+  /// Saves the image on disk.
+  /// \param filePath Path to where to save the image.
+  /// \param reverse Reverse the image when saving.
   void save(const std::string& filePath, bool reverse = false) const;
 
 private:
+  /// Reads a PNG image to memory.
+  /// \param file File to read.
+  /// \param reverse Reverse the image when reading.
   void readPng(std::ifstream& file, bool reverse);
+  /// Saves the image on disk in PNG.
+  /// \param file File to save.
+  /// \param reverse Reverse the image when saving.
   void savePng(std::ofstream& file, bool reverse) const;
+  /// Reads a TGA image to memory.
+  /// \param file File to read.
   void readTga(std::ifstream& file);
-  //void saveTga(std::ofstream& file) const;
+  /*
+  /// Saves the image on disk in TGA.
+  /// \param file File to save.
+  void saveTga(std::ofstream& file) const;
+  */
 
   unsigned int m_width {};
   unsigned int m_height {};
