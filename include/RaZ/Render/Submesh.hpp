a80ee5ff12deb085d290d44d815c3e3be487c53f
Razakhel <romain.milbert@gmail.com>
2019-04-15 18:25:13 +0200

    [Render/Submesh] A Submesh can be rendered as a set of points
    - Submesh contains a render function, used to render itself with different setups
    - Submesh now has an IndexBuffer as member instead of being contained into its VAO
    
    Signed-off-by: Razakhel <romain.milbert@gmail.com>

--- include/RaZ/Render/Submesh.hpp
+++ include/RaZ/Render/Submesh.hpp
@@ -3,6 +3,7 @@
 #ifndef RAZ_SUBMESH_HPP
 #define RAZ_SUBMESH_HPP
 
+#include <functional>
 #include <memory>
 
 #include "RaZ/Render/GraphicObjects.hpp"
@@ -11,7 +12,7 @@ namespace Raz {
 
 enum class RenderMode : unsigned int {
   POINT    = 0, // GL_POINTS
-  LINE     = 1, // GL_LINES
+  //LINE     = 1, // GL_LINES
   TRIANGLE = 4  // GL_TRIANGLES
 };
 
@@ -20,24 +21,23 @@ using SubmeshPtr = std::unique_ptr<Submesh>;
 
 class Submesh {
 public:
-  Submesh() = default;
-  explicit Submesh(RenderMode renderMode) : m_renderMode{ renderMode } {}
+  explicit Submesh(RenderMode renderMode = RenderMode::TRIANGLE) { setRenderMode(renderMode); }
   Submesh(const Submesh&) = delete;
-  Submesh(Submesh&&) noexcept = default;
+  Submesh(Submesh&&) = default;
 
   const std::vector<Vertex>& getVertices() const { return m_vbo.getVertices(); }
   std::vector<Vertex>& getVertices() { return m_vbo.getVertices(); }
   std::size_t getVertexCount() const { return getVertices().size(); }
-  const std::vector<unsigned int>& getTriangleIndices() const { return m_vao.getEbo().getTriangleIndices(); }
-  std::vector<unsigned int>& getTriangleIndices() { return m_vao.getEbo().getTriangleIndices(); }
-  const std::vector<unsigned int>& getLineIndices() const { return m_vao.getEbo().getLineIndices(); }
-  std::vector<unsigned int>& getLineIndices() { return m_vao.getEbo().getLineIndices(); }
-  std::size_t getTriangleIndexCount() const { return getLineIndices().size(); }
+  const std::vector<unsigned int>& getLineIndices() const { return m_ibo.getLineIndices(); }
+  std::vector<unsigned int>& getLineIndices() { return m_ibo.getLineIndices(); }
   std::size_t getLineIndexCount() const { return getLineIndices().size(); }
+  const std::vector<unsigned int>& getTriangleIndices() const { return m_ibo.getTriangleIndices(); }
+  std::vector<unsigned int>& getTriangleIndices() { return m_ibo.getTriangleIndices(); }
+  std::size_t getTriangleIndexCount() const { return getTriangleIndices().size(); }
   RenderMode getRenderMode() const { return m_renderMode; }
   std::size_t getMaterialIndex() const { return m_materialIndex; }
 
-  void setRenderMode(RenderMode renderMode) { m_renderMode = renderMode; }
+  void setRenderMode(RenderMode renderMode);
   void setMaterialIndex(std::size_t materialIndex) { m_materialIndex = materialIndex; }
 
   template <typename... Args>
@@ -57,7 +57,9 @@ private:
 
   VertexArray m_vao {};
   VertexBuffer m_vbo {};
+  IndexBuffer m_ibo {};
   RenderMode m_renderMode = RenderMode::TRIANGLE;
+  std::function<void(const Submesh&)> m_renderFunc {};
 
   std::size_t m_materialIndex {};
 };
--- src/RaZ/Render/Submesh.cpp
+++ src/RaZ/Render/Submesh.cpp
@@ -1,6 +1,38 @@
+#include "GL/glew.h"
 #include "RaZ/Render/Submesh.hpp"
 
 namespace Raz {
+
+void Submesh::setRenderMode(RenderMode renderMode) {
+  m_renderMode = renderMode;
+
+  switch (m_renderMode) {
+    case RenderMode::POINT:
+      m_renderFunc = [] (const Submesh& submesh) { glDrawArrays(GL_POINTS, 0, submesh.getVertexCount()); };
+      break;
+
+//    case RenderMode::LINE: {
+//      if (m_ibo.getLineIndices().empty())
+//        computeLineIndices();
+//
+//      m_renderFunc = [] (const Submesh& submesh) {
+//        glDrawElements(GL_LINES, static_cast<int>(submesh.getLineIndexCount()), GL_UNSIGNED_INT, nullptr);
+//      };
+//
+//      break;
+//    }
+
+    case RenderMode::TRIANGLE: {
+      m_renderFunc = [] (const Submesh& submesh) {
+        glDrawElements(GL_TRIANGLES, static_cast<int>(submesh.getTriangleIndexCount()), GL_UNSIGNED_INT, nullptr);
+      };
+
+      break;
+    }
+  }
+
+  loadIndices();
+}
 
 void Submesh::load() const {
   loadVertices();
@@ -9,30 +41,23 @@ void Submesh::load() const {
 
 void Submesh::draw() const {
   m_vao.bind();
-  glDrawElements(GL_TRIANGLES, static_cast<int>(getIndexCount()), GL_UNSIGNED_INT, nullptr);
+  m_ibo.bind();
+
+  m_renderFunc(*this);
 }
 
 void Submesh::loadVertices() const {
   m_vao.bind();
-
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-               static_cast<int64_t>(sizeof(getIndices().front()) * getIndices().size()),
-               getIndices().data(),
-               GL_STATIC_DRAW);
-
-  m_vao.unbind();
-}
-
-void Submesh::loadIndices() const {
-  m_vao.bind();
   m_vbo.bind();
 
+  const std::vector<Vertex>& vertices = getVertices();
+
   glBufferData(GL_ARRAY_BUFFER,
-               static_cast<int64_t>(sizeof(getVertices().front()) * getVertices().size()),
-               getVertices().data(),
+               static_cast<int64_t>(sizeof(vertices.front()) * vertices.size()),
+               vertices.data(),
                GL_STATIC_DRAW);
 
-  constexpr uint8_t stride = sizeof(getVertices().front());
+  constexpr uint8_t stride = sizeof(vertices.front());
 
   glVertexAttribPointer(0, 3,
                         GL_FLOAT, GL_FALSE,
@@ -40,21 +65,21 @@ void Submesh::loadIndices() const {
                         nullptr);
   glEnableVertexAttribArray(0);
 
-  constexpr std::size_t positionSize = sizeof(getVertices().front().position);
+  constexpr std::size_t positionSize = sizeof(vertices.front().position);
   glVertexAttribPointer(1, 2,
                         GL_FLOAT, GL_FALSE,
                         stride,
                         reinterpret_cast<void*>(positionSize));
   glEnableVertexAttribArray(1);
 
-  constexpr std::size_t texcoordsSize = sizeof(getVertices().front().texcoords);
+  constexpr std::size_t texcoordsSize = sizeof(vertices.front().texcoords);
   glVertexAttribPointer(2, 3,
                         GL_FLOAT, GL_FALSE,
                         stride,
                         reinterpret_cast<void*>(positionSize + texcoordsSize));
   glEnableVertexAttribArray(2);
 
-  constexpr std::size_t normalSize = sizeof(getVertices().front().normal);
+  constexpr std::size_t normalSize = sizeof(vertices.front().normal);
   glVertexAttribPointer(3, 3,
                         GL_FLOAT, GL_FALSE,
                         stride,
@@ -62,6 +87,22 @@ void Submesh::loadIndices() const {
   glEnableVertexAttribArray(3);
 
   m_vbo.unbind();
+  m_vao.unbind();
+}
+
+void Submesh::loadIndices() const {
+  m_vao.bind();
+  m_ibo.bind();
+
+  // Mapping the indices to lines' if asked, and triangles' otherwise
+  const std::vector<unsigned int>& indices = (/*m_renderMode == RenderMode::LINE ? getLineIndices() : */getTriangleIndices());
+
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+               static_cast<int64_t>(sizeof(indices.front()) * indices.size()),
+               indices.data(),
+               GL_STATIC_DRAW);
+
+  m_ibo.unbind();
   m_vao.unbind();
 }
 
