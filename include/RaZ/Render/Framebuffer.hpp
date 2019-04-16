a7b144c56661769416a6bc101b01029ce39a9c5c
Razakhel <romain.milbert@gmail.com>
2019-04-16 19:03:23 +0200

    [Render/Framebuffer] Externalized the buffers mapping
    - A new function Framebuffer::mapBuffers() is now available to be used separately
    - Resizing the framebuffer or moving it calls the buffers mapping
    - Framebuffer::assignVertexShader() is now static
    
    Signed-off-by: Razakhel <romain.milbert@gmail.com>

--- include/RaZ/Render/Framebuffer.hpp
+++ include/RaZ/Render/Framebuffer.hpp
@@ -14,7 +14,7 @@ class Framebuffer {
 public:
   Framebuffer();
   explicit Framebuffer(ShaderProgram& program);
-  Framebuffer(unsigned int width, unsigned int height, ShaderProgram& program);
+  Framebuffer(unsigned int width, unsigned int height, ShaderProgram& program) : Framebuffer(program) { resize(width, height); }
   Framebuffer(const Framebuffer&) = delete;
   Framebuffer(Framebuffer&& fbo) noexcept;
 
@@ -22,9 +22,15 @@ public:
   const TexturePtr& getNormalBuffer() const { return m_normalBuffer; }
   const TexturePtr& getColorBuffer() const { return m_colorBuffer; }
 
+  /// Assigns a basic vertex shader to the given program, to display the framebuffer.
+  /// \param program Shader program to assign the vertex shader to.
+  static void assignVertexShader(ShaderProgram& program);
+
   /// Initializes the buffers uniforms' indices to be bound later.
   /// \param program Shader program to which to send the uniforms.
   void initBuffers(const ShaderProgram& program) const;
+  /// Maps the buffers textures onto the graphics card.
+  void mapBuffers() const;
   /// Binds the framebuffer and clears the color & depth buffers.
   void bind() const;
   /// Unbinds the framebuffer.
@@ -36,9 +42,6 @@ public:
   /// \param width Width to be resized to.
   /// \param height Height to be resized to.
   void resize(unsigned int width, unsigned int height);
-  /// Assigns a basic vertex shader to the given program, to display the framebuffer.
-  /// \param program Shader program to assign the vertex shader to.
-  void assignVertexShader(ShaderProgram& program) const;
 
   Framebuffer& operator=(const Framebuffer&) = delete;
   Framebuffer& operator=(Framebuffer&& fbo) noexcept;
@@ -47,9 +50,9 @@ public:
 
 private:
   unsigned int m_index {};
-  TexturePtr m_depthBuffer {};
-  TexturePtr m_normalBuffer {};
-  TexturePtr m_colorBuffer {};
+  TexturePtr m_depthBuffer  = Texture::create(0, 0, ImageColorspace::DEPTH);
+  TexturePtr m_normalBuffer = Texture::create(0, 0, ImageColorspace::RGB);
+  TexturePtr m_colorBuffer  = Texture::create(0, 0, ImageColorspace::RGBA);
 };
 
 } // namespace Raz
--- src/RaZ/Render/Framebuffer.cpp
+++ src/RaZ/Render/Framebuffer.cpp
@@ -14,73 +14,15 @@ Framebuffer::Framebuffer(ShaderProgram& program) : Framebuffer() {
   initBuffers(program);
 }
 
-Framebuffer::Framebuffer(unsigned int width, unsigned int height, ShaderProgram& program) : Framebuffer(program) {
-  resize(width, height);
-
-  constexpr std::array<GLenum, 2> colorBuffers = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
-
-  bind();
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depthBuffer->getIndex(), 0);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, colorBuffers[0], GL_TEXTURE_2D, m_normalBuffer->getIndex(), 0);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, colorBuffers[1], GL_TEXTURE_2D, m_colorBuffer->getIndex(), 0);
-
-  glDrawBuffers(2, colorBuffers.data());
-
-  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-    std::cerr << "Error: Framebuffer is not complete." << std::endl;
-  unbind();
-}
-
 Framebuffer::Framebuffer(Framebuffer&& fbo) noexcept
   : m_index{ std::exchange(fbo.m_index, GL_INVALID_INDEX) },
     m_depthBuffer{ std::move(fbo.m_depthBuffer) },
     m_normalBuffer{ std::move(fbo.m_normalBuffer) },
-    m_colorBuffer{ std::move(fbo.m_colorBuffer) } {}
-
-void Framebuffer::initBuffers(const ShaderProgram& program) const {
-  bind();
-
-  program.use();
-  program.sendUniform("uniSceneBuffers.depth",  0);
-  program.sendUniform("uniSceneBuffers.normal", 1);
-  program.sendUniform("uniSceneBuffers.color",  2);
-
-  unbind();
+    m_colorBuffer{ std::move(fbo.m_colorBuffer) } {
+  mapBuffers();
 }
 
-void Framebuffer::bind() const {
-  glBindFramebuffer(GL_FRAMEBUFFER, m_index);
-
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-}
-
-void Framebuffer::unbind() const {
-  glBindFramebuffer(GL_FRAMEBUFFER, 0);
-}
-
-void Framebuffer::display(const ShaderProgram& program) const {
-  glClear(GL_COLOR_BUFFER_BIT);
-
-  Texture::activate(0);
-  m_depthBuffer->bind();
-
-  Texture::activate(1);
-  m_normalBuffer->bind();
-
-  Texture::activate(2);
-  m_colorBuffer->bind();
-
-  program.use();
-  Mesh::drawUnitQuad();
-}
-
-void Framebuffer::resize(unsigned int width, unsigned int height) {
-  m_depthBuffer  = Texture::create(width, height, ImageColorspace::DEPTH);
-  m_normalBuffer = Texture::create(width, height, ImageColorspace::RGB);
-  m_colorBuffer  = Texture::create(width, height, ImageColorspace::RGBA);
-}
-
-void Framebuffer::assignVertexShader(ShaderProgram& program) const {
+void Framebuffer::assignVertexShader(ShaderProgram& program) {
   static const std::string vertSource = R"(
     #version 330 core
 
@@ -99,11 +41,76 @@ void Framebuffer::assignVertexShader(ShaderProgram& program) const {
   program.setVertexShader(VertexShader::loadFromSource(vertSource));
 }
 
+void Framebuffer::initBuffers(const ShaderProgram& program) const {
+  bind();
+
+  program.use();
+  program.sendUniform("uniSceneBuffers.depth",  0);
+  program.sendUniform("uniSceneBuffers.normal", 1);
+  program.sendUniform("uniSceneBuffers.color",  2);
+
+  unbind();
+}
+
+void Framebuffer::mapBuffers() const {
+  constexpr std::array<GLenum, 2> colorBuffers = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+
+  bind();
+
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depthBuffer->getIndex(), 0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, colorBuffers[0], GL_TEXTURE_2D, m_normalBuffer->getIndex(), 0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, colorBuffers[1], GL_TEXTURE_2D, m_colorBuffer->getIndex(), 0);
+
+  glDrawBuffers(colorBuffers.size(), colorBuffers.data());
+
+  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    std::cerr << "Error: Framebuffer is not complete." << std::endl;
+
+  unbind();
+}
+
+void Framebuffer::bind() const {
+  glBindFramebuffer(GL_FRAMEBUFFER, m_index);
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Framebuffer::unbind() const {
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void Framebuffer::display(const ShaderProgram& program) const {
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  program.use();
+
+  Texture::activate(0);
+  m_depthBuffer->bind();
+
+  Texture::activate(1);
+  m_normalBuffer->bind();
+
+  Texture::activate(2);
+  m_colorBuffer->bind();
+
+  Mesh::drawUnitQuad();
+}
+
+void Framebuffer::resize(unsigned int width, unsigned int height) {
+  m_depthBuffer  = Texture::create(width, height, ImageColorspace::DEPTH);
+  m_normalBuffer = Texture::create(width, height, ImageColorspace::RGB);
+  m_colorBuffer  = Texture::create(width, height, ImageColorspace::RGBA);
+
+  mapBuffers();
+}
+
 Framebuffer& Framebuffer::operator=(Framebuffer&& fbo) noexcept {
   std::swap(m_index, fbo.m_index);
   m_depthBuffer  = std::move(fbo.m_depthBuffer);
   m_normalBuffer = std::move(fbo.m_normalBuffer);
   m_colorBuffer  = std::move(fbo.m_colorBuffer);
+
+  mapBuffers();
 
   return *this;
 }
