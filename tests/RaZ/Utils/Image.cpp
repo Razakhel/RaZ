cd791b503b38b988827d565a504bc029b775fe4e
Razakhel <romain.milbert@gmail.com>
2019-06-19 19:06:38 +0200

    [Tests/Utils/Image] Reworked the PNG import test
    - It now uses a simpler PNG test file, and checks that the first pixels' values are as expected
    
    - Added a 2x2 checkerboard PNG test file

--- tests/RaZ/Utils/Image.cpp
+++ tests/RaZ/Utils/Image.cpp
@@ -20,13 +20,23 @@ TEST_CASE("Image manual creation") {
 }
 
 TEST_CASE("Image imported PNG") {
-  const Raz::Image img("../../assets/textures/default.png");
+  const Raz::Image img("../../tests/assets/images/defaultTest.png");
 
-  REQUIRE(img.getWidth() == 128);
-  REQUIRE(img.getHeight() == 128);
+  REQUIRE(img.getWidth() == 2);
+  REQUIRE(img.getHeight() == 2);
   REQUIRE(img.getColorspace() == Raz::ImageColorspace::RGBA);
   REQUIRE(img.getDataType() == Raz::ImageDataType::BYTE);
   REQUIRE_FALSE(img.isEmpty());
+
+  REQUIRE(*static_cast<const uint8_t*>(img.getDataPtr())       == 191);
+  REQUIRE(*(static_cast<const uint8_t*>(img.getDataPtr()) + 1) == 191);
+  REQUIRE(*(static_cast<const uint8_t*>(img.getDataPtr()) + 2) == 191);
+  REQUIRE(*(static_cast<const uint8_t*>(img.getDataPtr()) + 3) == 255);
+
+  REQUIRE(*(static_cast<const uint8_t*>(img.getDataPtr()) + 4) == 239);
+  REQUIRE(*(static_cast<const uint8_t*>(img.getDataPtr()) + 5) == 239);
+  REQUIRE(*(static_cast<const uint8_t*>(img.getDataPtr()) + 6) == 239);
+  REQUIRE(*(static_cast<const uint8_t*>(img.getDataPtr()) + 7) == 255);
 }
 
 TEST_CASE("Image imported TGA") {
