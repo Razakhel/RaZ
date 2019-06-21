6d51bd61dba1b802f6f5d5d97713990d69b369c5
Razakhel <romain.milbert@gmail.com>
2019-06-21 18:04:51 +0200

    [Render/Material] Reworked material classes
    - Factorized base color factor & texture into Material directly instead of one instance for each material type (diffuse/albedo)
    - Added an option to flip vertically the maps to load (true by default)
    
    - This will most likely evolve the same way in the future

--- include/RaZ/Render/Material.hpp
+++ include/RaZ/Render/Material.hpp
@@ -35,9 +35,19 @@ enum class MaterialPreset : uint16_t {
 
 class Material {
 public:
-  Material(const Material&) = default;
+  Material(Material&&) = default;
 
   virtual MaterialType getType() const = 0;
+  const Vec3f& getBaseColor() const { return m_baseColor; }
+
+  const TexturePtr& getBaseColorMap() const { return m_baseColorMap; }
+
+  void setBaseColor(float red, float green, float blue) { setBaseColor(Vec3f({ red, green, blue })); }
+  void setBaseColor(const Vec3f& color) { m_baseColor = color; }
+
+  void setBaseColorMap(TexturePtr baseColorMap) { m_baseColorMap = std::move(baseColorMap); }
+
+  void loadBaseColorMap(const std::string& fileName, bool flipVertically = true) { m_baseColorMap = Texture::create(fileName, flipVertically); }
 
   static MaterialCookTorrancePtr recoverMaterial(MaterialPreset preset, float roughnessFactor);
   virtual MaterialPtr clone() const = 0;
@@ -48,30 +58,36 @@ public:
 
 protected:
   Material() = default;
+  Material(const Material&) = default;
+  explicit Material(TexturePtr baseColorMap) : m_baseColorMap{ std::move(baseColorMap) } {}
+  explicit Material(const std::string& fileName, bool flipVertically = true) : Material(Texture::create(fileName, flipVertically)) {}
+
+  Vec3f m_baseColor = Vec3f(1.f);
+
+  TexturePtr m_baseColorMap = Texture::recoverTexture(TexturePreset::WHITE);
 };
 
 class MaterialBlinnPhong : public Material {
 public:
   MaterialBlinnPhong() = default;
-  explicit MaterialBlinnPhong(TexturePtr diffuseMap) : m_diffuseMap{ std::move(diffuseMap) } {}
-  explicit MaterialBlinnPhong(const std::string& fileName) : MaterialBlinnPhong(Texture::create(fileName)) {}
+  explicit MaterialBlinnPhong(TexturePtr diffuseMap) : Material(std::move(diffuseMap)) {}
+  explicit MaterialBlinnPhong(const std::string& fileName, bool flipVertically = true) : MaterialBlinnPhong(Texture::create(fileName, flipVertically)) {}
 
   MaterialType getType() const override { return MaterialType::BLINN_PHONG; }
   const Vec3f& getAmbient() const { return m_ambient; }
-  const Vec3f& getDiffuse() const { return m_diffuse; }
   const Vec3f& getSpecular() const { return m_specular; }
   const Vec3f& getEmissive() const { return m_emissive; }
   float getTransparency() const { return m_transparency; }
 
+  const TexturePtr& getDiffuseMap() const { return getBaseColorMap(); }
   const TexturePtr& getAmbientMap() const { return m_ambientMap; }
-  const TexturePtr& getDiffuseMap() const { return m_diffuseMap; }
   const TexturePtr& getSpecularMap() const { return m_specularMap; }
   const TexturePtr& getEmissiveMap() const { return m_emissiveMap; }
   const TexturePtr& getTransparencyMap() const { return m_transparencyMap; }
   const TexturePtr& getBumpMap() const { return m_bumpMap; }
 
-  void setDiffuse(float red, float green, float blue) { setDiffuse(Vec3f({ red, green, blue })); }
-  void setDiffuse(const Vec3f& val) { m_diffuse = val; }
+  void setDiffuse(float red, float green, float blue) { setBaseColor(red, green, blue); }
+  void setDiffuse(const Vec3f& color) { setBaseColor(color); }
   void setAmbient(float red, float green, float blue) { setAmbient(Vec3f({ red, green, blue })); }
   void setAmbient(const Vec3f& val) { m_ambient = val; }
   void setSpecular(float red, float green, float blue) { setSpecular(Vec3f({ red, green, blue })); }
@@ -80,22 +96,22 @@ public:
   void setEmissive(const Vec3f& val) { m_emissive = val; }
   void setTransparency(float transparency) { m_transparency = transparency; }
 
-  void setAmbientMap(const TexturePtr& ambientMap) { m_ambientMap = ambientMap; }
-  void setDiffuseMap(const TexturePtr& diffuseMap) { m_diffuseMap = diffuseMap; }
-  void setSpecularMap(const TexturePtr& specularMap) { m_specularMap = specularMap; }
-  void setEmissiveMap(const TexturePtr& emissiveMap) { m_emissiveMap = emissiveMap; }
-  void setTransparencyMap(const TexturePtr& transparencyMap) { m_transparencyMap = transparencyMap; }
-  void setBumpMap(const TexturePtr& bumpMap) { m_bumpMap = bumpMap; }
+  void setDiffuseMap(TexturePtr diffuseMap) { setBaseColorMap(std::move(diffuseMap)); }
+  void setAmbientMap(TexturePtr ambientMap) { m_ambientMap = std::move(ambientMap); }
+  void setSpecularMap(TexturePtr specularMap) { m_specularMap = std::move(specularMap); }
+  void setEmissiveMap(TexturePtr emissiveMap) { m_emissiveMap = std::move(emissiveMap); }
+  void setTransparencyMap(TexturePtr transparencyMap) { m_transparencyMap = std::move(transparencyMap); }
+  void setBumpMap(TexturePtr bumpMap) { m_bumpMap = std::move(bumpMap); }
 
   template <typename... Args>
   static MaterialBlinnPhongPtr create(Args&&... args) { return std::make_unique<MaterialBlinnPhong>(std::forward<Args>(args)...); }
 
-  void loadAmbientMap(const std::string& fileName) { m_ambientMap = Texture::create(fileName); }
-  void loadDiffuseMap(const std::string& fileName) { m_diffuseMap = Texture::create(fileName); }
-  void loadSpecularMap(const std::string& fileName) { m_specularMap = Texture::create(fileName); }
-  void loadEmissiveMap(const std::string& fileName) { m_emissiveMap = Texture::create(fileName); }
-  void loadTransparencyMap(const std::string& fileName) { m_transparencyMap = Texture::create(fileName); }
-  void loadBumpMap(const std::string& fileName) { m_bumpMap = Texture::create(fileName); }
+  void loadDiffuseMap(const std::string& fileName, bool flipVertically = true) { loadBaseColorMap(fileName, flipVertically); }
+  void loadAmbientMap(const std::string& fileName, bool flipVertically = true) { m_ambientMap = Texture::create(fileName, flipVertically); }
+  void loadSpecularMap(const std::string& fileName, bool flipVertically = true) { m_specularMap = Texture::create(fileName, flipVertically); }
+  void loadEmissiveMap(const std::string& fileName, bool flipVertically = true) { m_emissiveMap = Texture::create(fileName, flipVertically); }
+  void loadTransparencyMap(const std::string& fileName, bool flipVertically = true) { m_transparencyMap = Texture::create(fileName, flipVertically); }
+  void loadBumpMap(const std::string& fileName, bool flipVertically = true) { m_bumpMap = Texture::create(fileName, flipVertically); }
 
   MaterialPtr clone() const override { return MaterialBlinnPhong::create(*this); }
   void initTextures(const ShaderProgram& program) const override;
@@ -103,13 +119,11 @@ public:
 
 private:
   Vec3f m_ambient      = Vec3f(1.f);
-  Vec3f m_diffuse      = Vec3f(1.f);
   Vec3f m_specular     = Vec3f(1.f);
   Vec3f m_emissive     = Vec3f(0.f);
   float m_transparency = 1.f;
 
   TexturePtr m_ambientMap      = Texture::recoverTexture(TexturePreset::WHITE);
-  TexturePtr m_diffuseMap      = Texture::recoverTexture(TexturePreset::WHITE);
   TexturePtr m_specularMap     = Texture::recoverTexture(TexturePreset::WHITE);
   TexturePtr m_emissiveMap     = Texture::recoverTexture(TexturePreset::WHITE);
   TexturePtr m_transparencyMap = Texture::recoverTexture(TexturePreset::WHITE);
@@ -119,52 +133,47 @@ private:
 class MaterialCookTorrance : public Material {
 public:
   MaterialCookTorrance() = default;
-  explicit MaterialCookTorrance(TexturePtr albedoMap) : m_albedoMap{ std::move(albedoMap) } {}
-  explicit MaterialCookTorrance(const std::string& fileName) : MaterialCookTorrance(Texture::create(fileName)) {}
+  explicit MaterialCookTorrance(TexturePtr albedoMap) : Material(std::move(albedoMap)) {}
+  explicit MaterialCookTorrance(const std::string& fileName, bool flipVertically = true) : MaterialCookTorrance(Texture::create(fileName, flipVertically)) {}
   MaterialCookTorrance(const Vec3f& baseColor, float metallicFactor, float roughnessFactor)
-    : m_baseColor{ baseColor }, m_metallicFactor{ metallicFactor }, m_roughnessFactor{ roughnessFactor } {}
+    : m_metallicFactor{ metallicFactor }, m_roughnessFactor{ roughnessFactor } { setBaseColor(baseColor); }
 
   MaterialType getType() const override { return MaterialType::COOK_TORRANCE; }
-  const Vec3f& getBaseColor() const { return m_baseColor; }
   float getMetallicFactor() const { return m_metallicFactor; }
   float getRoughnessFactor() const { return m_roughnessFactor; }
 
-  const TexturePtr& getAlbedoMap() const { return m_albedoMap; }
+  const TexturePtr& getAlbedoMap() const { return getBaseColorMap(); }
   const TexturePtr& getMetallicMap() const { return m_metallicMap; }
   const TexturePtr& getNormalMap() const { return m_normalMap; }
   const TexturePtr& getRoughnessMap() const { return m_roughnessMap; }
   const TexturePtr& getAmbientOcclusionMap() const { return m_ambientOcclusionMap; }
 
-  void setBaseColor(float red, float green, float blue) { setBaseColor(Vec3f({ red, green, blue })); }
-  void setBaseColor(const Vec3f& color) { m_baseColor = color; }
   void setMetallicFactor(float metallicFactor) { m_metallicFactor = metallicFactor; }
   void setRoughnessFactor(float roughnessFactor) { m_roughnessFactor = roughnessFactor; }
 
-  void setAlbedoMap(const TexturePtr& albedoMap) { m_albedoMap = albedoMap; }
-  void setNormalMap(const TexturePtr& normalMap) { m_normalMap = normalMap; }
-  void setMetallicMap(const TexturePtr& metallicMap) { m_metallicMap = metallicMap; }
-  void setRoughnessMap(const TexturePtr& roughnessMap) { m_roughnessMap = roughnessMap; }
-  void setAmbientOcclusionMap(const TexturePtr& ambientOcclusionMap) { m_ambientOcclusionMap = ambientOcclusionMap; }
+  void setAlbedoMap(TexturePtr albedoMap) { setBaseColorMap(std::move(albedoMap)); }
+  void setNormalMap(TexturePtr normalMap) { m_normalMap = std::move(normalMap); }
+  void setMetallicMap(TexturePtr metallicMap) { m_metallicMap = std::move(metallicMap); }
+  void setRoughnessMap(TexturePtr roughnessMap) { m_roughnessMap = std::move(roughnessMap); }
+  void setAmbientOcclusionMap(TexturePtr ambientOcclusionMap) { m_ambientOcclusionMap = std::move(ambientOcclusionMap); }
 
   template <typename... Args>
   static MaterialCookTorrancePtr create(Args&&... args) { return std::make_unique<MaterialCookTorrance>(std::forward<Args>(args)...); }
 
-  void loadAlbedoMap(const std::string& fileName) { m_albedoMap = Texture::create(fileName); }
-  void loadNormalMap(const std::string& fileName) { m_normalMap = Texture::create(fileName); }
-  void loadMetallicMap(const std::string& fileName) { m_metallicMap = Texture::create(fileName); }
-  void loadRoughnessMap(const std::string& fileName) { m_roughnessMap = Texture::create(fileName); }
-  void loadAmbientOcclusionMap(const std::string& fileName) { m_ambientOcclusionMap = Texture::create(fileName); }
+  void loadAlbedoMap(const std::string& fileName, bool flipVertically = true) { loadBaseColorMap(fileName, flipVertically); }
+  void loadNormalMap(const std::string& fileName, bool flipVertically = true) { m_normalMap = Texture::create(fileName, flipVertically); }
+  void loadMetallicMap(const std::string& fileName, bool flipVertically = true) { m_metallicMap = Texture::create(fileName, flipVertically); }
+  void loadRoughnessMap(const std::string& fileName, bool flipVertically = true) { m_roughnessMap = Texture::create(fileName, flipVertically); }
+  void loadAmbientOcclusionMap(const std::string& fileName, bool flipVertically = true) { m_ambientOcclusionMap = Texture::create(fileName, flipVertically); }
 
   MaterialPtr clone() const override { return MaterialCookTorrance::create(*this); }
   void initTextures(const ShaderProgram& program) const override;
   void bindAttributes(const ShaderProgram& program) const override;
 
 private:
-  Vec3f m_baseColor       = Vec3f(1.f);
   float m_metallicFactor  = 1.f;
   float m_roughnessFactor = 1.f;
 
-  TexturePtr m_albedoMap           = Texture::recoverTexture(TexturePreset::WHITE);
   TexturePtr m_normalMap           = Texture::recoverTexture(TexturePreset::WHITE);
   TexturePtr m_metallicMap         = Texture::recoverTexture(TexturePreset::WHITE);
   TexturePtr m_roughnessMap        = Texture::recoverTexture(TexturePreset::WHITE);
