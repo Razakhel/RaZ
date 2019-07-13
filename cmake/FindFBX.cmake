cd5fc166bfc42d6d44ffef9bf97e02e13c5b50ae
Razakhel <romain.milbert@gmail.com>
2019-07-13 09:16:22 +0200

    [CMake/FBX] Reworked the whole file, which wasn't correct for VS
    - Build type is now only chosen when with GCC
    - The $(Configuration) variable is inserted for MSVC to replace it internally

--- cmake/FindFBX.cmake
+++ cmake/FindFBX.cmake
@@ -2,50 +2,67 @@
 # Finding FBX SDK #
 ###################
 
-if ("${CMAKE_BUILD_TYPE}" MATCHES "(D|d)eb")
-    set(BUILD_TYPE "debug")
-else ()
-    set(BUILD_TYPE "release")
-endif ()
-
 if (MSVC)
-    # Wildcards used to fetch from 'Program Files (x86)' as well, plus for every version: 2019.0, 2018.1.1, 2018.0, ...
-    # TODO: minor, but should be sorted in reverse order: the more recent the better (always possible to manually select it)
-    set(FBX_PATH "Program Files*/Autodesk/FBX/FBX SDK/*")
-    file(
-        GLOB
-        SEARCH_PATHS
-
-        "C:/${FBX_PATH}"
-        "D:/${FBX_PATH}"
-        "E:/${FBX_PATH}"
-    )
-
     # TODO: searching by default into 'vs2015'; should be MSVC-version dependent
-    set(FBX_LIB_DIR "lib/vs2015/x64/${BUILD_TYPE}")
-    set(FBX_LIB_SUBPATH "${FBX_LIB_DIR}/libfbxsdk.lib")
+    set(FBX_LIB_DIR "${FBX_ROOT_DIR}/lib/vs2015/x64")
+
+    set(FBX_LIB_NAME "libfbxsdk")
+    set(FBX_DEBUG_LIB "${FBX_LIB_DIR}/debug/${FBX_LIB_NAME}")
+    set(FBX_RELEASE_LIB "${FBX_LIB_DIR}/release/${FBX_LIB_NAME}")
+
+    # Checking that .lib & .dll exist, either in debug or release
+    if ((EXISTS "${FBX_DEBUG_LIB}.lib" AND EXISTS "${FBX_DEBUG_LIB}.dll") OR (EXISTS "${FBX_RELEASE_LIB}.lib" AND EXISTS "${FBX_RELEASE_LIB}.dll"))
+        # The $(Configuration) macro is later replaced by VS automatically according to the current configuration (Debug, Release, etc)
+        set(FBX_LIB "${FBX_LIB_DIR}/$(Configuration)/${FBX_LIB_NAME}.lib")
+
+        set(FBX_MSVC_DLL "${FBX_LIB_DIR}/$(Configuration)/${FBX_LIB_NAME}.dll")
+        set(FBX_CONFIG_DLL "${FBX_LIB_DIR}/$<CONFIG>/${FBX_LIB_NAME}.dll")
+
+        set(FBX_FOUND TRUE)
+    endif ()
 elseif (CMAKE_COMPILER_IS_GNUCC)
-    file(
-        GLOB
-        SEARCH_PATHS
+    if ("${CMAKE_BUILD_TYPE}" MATCHES "(D|d)eb")
+        set(BUILD_TYPE "debug")
+    else ()
+        set(BUILD_TYPE "release")
+    endif ()
 
-        "/usr"
-        "/usr/local"
-        "~/*FBX*"
-        "~/*fbx*"
-    )
+    set(FBX_LIB "${FBX_ROOT_DIR}/lib/gcc4/x64/${BUILD_TYPE}/libfbxsdk.a")
 
-    set(FBX_LIB_DIR "lib/gcc4/x64/${BUILD_TYPE}")
-    set(FBX_LIB_SUBPATH "${FBX_LIB_DIR}/libfbxsdk.a")
+    # Checking if correctly user-assigned or previously found
+    if (EXISTS "${FBX_ROOT_DIR}/include/fbxsdk.h" AND EXISTS "${FBX_LIB}")
+        set(FBX_FOUND TRUE)
+    endif ()
 endif ()
 
-# Checking if correctly user-assigned or previously found
-if (EXISTS ${FBX_ROOT_DIR}/include/fbxsdk.h AND EXISTS ${FBX_ROOT_DIR}/${FBX_LIB_SUBPATH})
-    set(FBX_FOUND TRUE)
-    set(FBX_LIB ${FBX_ROOT_DIR}/${FBX_LIB_SUBPATH})
-else () # Not existing at requested place, searching for it elsewhere
+# SDK not found yet at the requested place, searching for it elsewhere
+if (NOT FBX_FOUND)
+    if (MSVC)
+        # Wildcards used to fetch from 'Program Files (x86)' as well, plus for every version: 2019.0, 2018.1.1, 2018.0, ...
+        # TODO: minor, but should be sorted in reverse order: the more recent the better (always possible to manually select it)
+        set(FBX_PATH "Program Files*/Autodesk/FBX/FBX SDK/*")
+        file(
+            GLOB
+            SEARCH_PATHS
+
+            "C:/${FBX_PATH}"
+            "D:/${FBX_PATH}"
+            "E:/${FBX_PATH}"
+        )
+    elseif (CMAKE_COMPILER_IS_GNUC)
+        file(
+            GLOB
+            SEARCH_PATHS
+
+            "/usr"
+            "/usr/local"
+            "~/*FBX*"
+            "~/*fbx*"
+        )
+    endif ()
+
     foreach (PATH ${SEARCH_PATHS})
-        set(FBX_LIB_LOCATION "${PATH}/${FBX_LIB_SUBPATH}")
+        set(FBX_LIB_LOCATION "${PATH}/${FBX_LIB_DIR}")
 
         # If header & lib exist, include & link each other respectively
         if (EXISTS "${PATH}/include/fbxsdk.h" AND EXISTS ${FBX_LIB_LOCATION})
