d45c877c33b31c8c8cf5e72b178cc67114156b4a
Razakhel <romain.milbert@gmail.com>
2019-09-10 18:12:58 +0200

    [Utils/FloatUtils] Added an absolute tolerance param for near-equality
    - This is defaulted to epsilon, which keeps the current behavior

--- include/RaZ/Utils/FloatUtils.hpp
+++ include/RaZ/Utils/FloatUtils.hpp
@@ -8,27 +8,26 @@
 #include <limits>
 #include <type_traits>
 
-namespace Raz {
-
-namespace FloatUtils {
+namespace Raz::FloatUtils {
 
 /// Checks if two given floating point values are nearly equal to each other.
 /// \tparam T Type of both values to check; it must be a floating point type.
+/// \tparam TolT Tolerance type, which may differ from the values type; it must be a floating point type.
 /// \param val1 First value to compare.
 /// \param val2 Second value to compare.
-/// \return True if values are nearly equal, false otherwise.
-template <typename T>
-inline constexpr bool areNearlyEqual(T val1, T val2) {
-  static_assert(std::is_floating_point_v<T>, "Error: Values' type must be floating point.");
+/// \param absTol Absolute tolerance to compare the values with.
+/// \return True if values are nearly equal to each other, false otherwise.
+template <typename T, typename TolT = T>
+inline constexpr bool areNearlyEqual(T val1, T val2, TolT absTol = std::numeric_limits<TolT>::epsilon()) {
+  static_assert(std::is_floating_point_v<T>, "Error: Values type must be floating point.");
+  static_assert(std::is_floating_point_v<TolT>, "Error: Tolerance type must be floating point.");
 
   // Using absolute & relative tolerances for floating points types: http://www.realtimecollisiondetection.net/pubs/Tolerances/
   // Could be a better idea to use ULPs checking. May be slower though?
   const T absDiff = std::abs(val1 - val2);
-  return (absDiff <= std::numeric_limits<T>::epsilon() * std::max({ static_cast<T>(1), std::abs(val1), std::abs(val2) }));
+  return (absDiff <= absTol * std::max({ static_cast<T>(1), std::abs(val1), std::abs(val2) }));
 }
 
-} // namespace FloatUtils
-
-} // namespace Raz
+} // namespace Raz::FloatUtils
 
 #endif // RAZ_FLOATUTILS_HPP
