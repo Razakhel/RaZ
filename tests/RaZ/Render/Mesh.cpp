d44c2d231b6c199a2efe77803d98f131b068d9bb
Razakhel <romain.milbert@gmail.com>
2020-01-09 21:30:08 +0100

    [Tests/Render/Mesh] Added a Cook-Torrance OBJ import unit test
    - This checks that a given OBJ with a Cook-Torrance material is imported as expected
    
    - Added missed texture values in the Blinn-Phong import test
    - Fixed a wrong texture path in the Cook-Torrance's MTL file

--- tests/RaZ/Render/Mesh.cpp
+++ tests/RaZ/Render/Mesh.cpp
@@ -29,7 +29,12 @@ TEST_CASE("Mesh imported OBJ cube (Blinn-Phong)") {
   CHECK(material.getSpecular() == Raz::Vec3f(0.33f));
   CHECK(material.getEmissive() == Raz::Vec3f(0.f));
 
-  // Each texture is flipped vertically when imported; the values are checked accordingly
+  // Each texture is flipped vertically when imported; the values are checked accordingly:
+  //    ---------
+  //    | 3 | 4 |
+  //    |-------|
+  //    | 1 | 2 |
+  //    ---------
 
   // Diffuse map
   {
@@ -44,6 +49,12 @@ TEST_CASE("Mesh imported OBJ cube (Blinn-Phong)") {
     const auto* diffuseData = static_cast<const uint8_t*>(material.getDiffuseMap()->getImage().getDataPtr());
 
     // RGBR image with alpha, flipped vertically: verifying that values are BRRG with 50% opacity
+
+    // ---------
+    // | R | G |
+    // |-------|
+    // | B | R |
+    // ---------
 
     CHECK(diffuseData[0] == 0);
     CHECK(diffuseData[1] == 0);
@@ -78,6 +89,12 @@ TEST_CASE("Mesh imported OBJ cube (Blinn-Phong)") {
 
     const auto* ambientData = static_cast<const uint8_t*>(material.getAmbientMap()->getImage().getDataPtr());
 
+    // ---------
+    // | B | B |
+    // |-------|
+    // | B | B |
+    // ---------
+
     CHECK(ambientData[0] == 0);
     CHECK(ambientData[1] == 0);
     CHECK(ambientData[2] == 255);
@@ -89,6 +106,10 @@ TEST_CASE("Mesh imported OBJ cube (Blinn-Phong)") {
     CHECK(ambientData[6] == 0);
     CHECK(ambientData[7] == 0);
     CHECK(ambientData[8] == 255);
+
+    CHECK(ambientData[9]  == 0);
+    CHECK(ambientData[10] == 0);
+    CHECK(ambientData[11] == 255);
   }
 
   // Specular map
@@ -102,6 +123,12 @@ TEST_CASE("Mesh imported OBJ cube (Blinn-Phong)") {
     REQUIRE(material.getSpecularMap()->getImage().getDataType() == Raz::ImageDataType::BYTE);
 
     const auto* specularData = static_cast<const uint8_t*>(material.getSpecularMap()->getImage().getDataPtr());
+
+    // ---------
+    // | X |   |
+    // |-------|
+    // |   | X |
+    // ---------
 
     CHECK(specularData[0] == 0);
     CHECK(specularData[1] == 255);
@@ -121,6 +148,12 @@ TEST_CASE("Mesh imported OBJ cube (Blinn-Phong)") {
 
     const auto* emissiveData = static_cast<const uint8_t*>(material.getEmissiveMap()->getImage().getDataPtr());
 
+    // ---------
+    // | R | R |
+    // |-------|
+    // | R | R |
+    // ---------
+
     CHECK(emissiveData[0] == 255);
     CHECK(emissiveData[1] == 0);
     CHECK(emissiveData[2] == 0);
@@ -132,6 +165,10 @@ TEST_CASE("Mesh imported OBJ cube (Blinn-Phong)") {
     CHECK(emissiveData[6] == 255);
     CHECK(emissiveData[7] == 0);
     CHECK(emissiveData[8] == 0);
+
+    CHECK(emissiveData[9]  == 255);
+    CHECK(emissiveData[10] == 0);
+    CHECK(emissiveData[11] == 0);
   }
 
   // Transparency map
@@ -145,6 +182,12 @@ TEST_CASE("Mesh imported OBJ cube (Blinn-Phong)") {
     REQUIRE(material.getTransparencyMap()->getImage().getDataType() == Raz::ImageDataType::BYTE);
 
     const auto* transparencyData = static_cast<const uint8_t*>(material.getTransparencyMap()->getImage().getDataPtr());
+
+    // ---------
+    // | X | X |
+    // |-------|
+    // | X | X |
+    // ---------
 
     CHECK(transparencyData[0] == 255);
     CHECK(transparencyData[1] == 255);
@@ -164,10 +207,188 @@ TEST_CASE("Mesh imported OBJ cube (Blinn-Phong)") {
 
     const auto* bumpData = static_cast<const uint8_t*>(material.getBumpMap()->getImage().getDataPtr());
 
+    // ---------
+    // |   |   |
+    // |-------|
+    // |   |   |
+    // ---------
+
     CHECK(bumpData[0] == 0);
     CHECK(bumpData[1] == 0);
     CHECK(bumpData[2] == 0);
     CHECK(bumpData[3] == 0);
+  }
+}
+
+TEST_CASE("Mesh imported OBJ (Cook-Torrance)") {
+  const Raz::Mesh mesh(RAZ_TESTS_ROOT + "assets/meshes/cube_CT.obj"s);
+
+  CHECK(mesh.getSubmeshes().size() == 1);
+  CHECK(mesh.recoverVertexCount() == 24);
+  CHECK(mesh.recoverTriangleCount() == 12);
+
+  REQUIRE(mesh.getMaterials().size() == 1);
+  REQUIRE(mesh.getMaterials().front()->getType() == Raz::MaterialType::COOK_TORRANCE);
+
+  const auto& material = static_cast<const Raz::MaterialCookTorrance&>(*mesh.getMaterials().front());
+
+  CHECK(material.getBaseColor() == Raz::Vec3f(1.f));
+  CHECK(material.getMetallicFactor() == 1.f);
+  CHECK(material.getRoughnessFactor() == 1.f);
+
+  // Each texture is flipped vertically when imported; the values are checked accordingly:
+  //    ---------
+  //    | 3 | 4 |
+  //    |-------|
+  //    | 1 | 2 |
+  //    ---------
+
+  // Albedo map
+  {
+    REQUIRE_FALSE(material.getAlbedoMap()->getImage().isEmpty());
+
+    CHECK(material.getAlbedoMap()->getImage().getColorspace() == Raz::ImageColorspace::RGBA);
+    CHECK(material.getAlbedoMap()->getImage().getWidth() == 2);
+    CHECK(material.getAlbedoMap()->getImage().getHeight() == 2);
+
+    REQUIRE(material.getAlbedoMap()->getImage().getDataType() == Raz::ImageDataType::BYTE);
+
+    const auto* albedoData = static_cast<const uint8_t*>(material.getAlbedoMap()->getImage().getDataPtr());
+
+    // RGBR image with alpha, flipped vertically: verifying that values are BRRG with 50% opacity
+
+    // ---------
+    // | R | G |
+    // |-------|
+    // | B | R |
+    // ---------
+
+    CHECK(albedoData[0] == 0);
+    CHECK(albedoData[1] == 0);
+    CHECK(albedoData[2] == 255);
+    CHECK(albedoData[3] == 127);
+
+    CHECK(albedoData[4] == 255);
+    CHECK(albedoData[5] == 0);
+    CHECK(albedoData[6] == 0);
+    CHECK(albedoData[7] == 127);
+
+    CHECK(albedoData[8]  == 255);
+    CHECK(albedoData[9]  == 0);
+    CHECK(albedoData[10] == 0);
+    CHECK(albedoData[11] == 127);
+
+    CHECK(albedoData[12] == 0);
+    CHECK(albedoData[13] == 255);
+    CHECK(albedoData[14] == 0);
+    CHECK(albedoData[15] == 127);
+  }
+
+  // Normal map
+  {
+    REQUIRE_FALSE(material.getNormalMap()->getImage().isEmpty());
+
+    CHECK(material.getNormalMap()->getImage().getColorspace() == Raz::ImageColorspace::RGB);
+    CHECK(material.getNormalMap()->getImage().getWidth() == 2);
+    CHECK(material.getNormalMap()->getImage().getHeight() == 2);
+
+    REQUIRE(material.getNormalMap()->getImage().getDataType() == Raz::ImageDataType::BYTE);
+
+    const auto* normalData = static_cast<const uint8_t*>(material.getNormalMap()->getImage().getDataPtr());
+
+    // ---------
+    // | B | B |
+    // |-------|
+    // | B | B |
+    // ---------
+
+    CHECK(normalData[0] == 0);
+    CHECK(normalData[1] == 0);
+    CHECK(normalData[2] == 255);
+
+    CHECK(normalData[3] == 0);
+    CHECK(normalData[4] == 0);
+    CHECK(normalData[5] == 255);
+
+    CHECK(normalData[6] == 0);
+    CHECK(normalData[7] == 0);
+    CHECK(normalData[8] == 255);
+
+    CHECK(normalData[9]  == 0);
+    CHECK(normalData[10] == 0);
+    CHECK(normalData[11] == 255);
+  }
+
+  // Metallic map
+  {
+    REQUIRE_FALSE(material.getMetallicMap()->getImage().isEmpty());
+
+    CHECK(material.getMetallicMap()->getImage().getColorspace() == Raz::ImageColorspace::GRAY);
+    CHECK(material.getMetallicMap()->getImage().getWidth() == 2);
+    CHECK(material.getMetallicMap()->getImage().getHeight() == 2);
+
+    REQUIRE(material.getMetallicMap()->getImage().getDataType() == Raz::ImageDataType::BYTE);
+
+    const auto* metallicData = static_cast<const uint8_t*>(material.getMetallicMap()->getImage().getDataPtr());
+
+    // ---------
+    // | X | X |
+    // |-------|
+    // | X | X |
+    // ---------
+
+    CHECK(metallicData[0] == 255);
+    CHECK(metallicData[1] == 255);
+    CHECK(metallicData[2] == 255);
+    CHECK(metallicData[3] == 255);
+  }
+
+  // Roughness map
+  {
+    REQUIRE_FALSE(material.getRoughnessMap()->getImage().isEmpty());
+
+    CHECK(material.getRoughnessMap()->getImage().getColorspace() == Raz::ImageColorspace::GRAY);
+    CHECK(material.getRoughnessMap()->getImage().getWidth() == 2);
+    CHECK(material.getRoughnessMap()->getImage().getHeight() == 2);
+
+    REQUIRE(material.getRoughnessMap()->getImage().getDataType() == Raz::ImageDataType::BYTE);
+
+    const auto* roughnessData = static_cast<const uint8_t*>(material.getRoughnessMap()->getImage().getDataPtr());
+
+    // ---------
+    // |   |   |
+    // |-------|
+    // |   |   |
+    // ---------
+
+    CHECK(roughnessData[0] == 0);
+    CHECK(roughnessData[1] == 0);
+    CHECK(roughnessData[2] == 0);
+    CHECK(roughnessData[3] == 0);
+  }
+
+  // Ambient occlusion map
+  {
+    REQUIRE_FALSE(material.getAmbientOcclusionMap()->getImage().isEmpty());
+
+    CHECK(material.getAmbientOcclusionMap()->getImage().getColorspace() == Raz::ImageColorspace::GRAY);
+    CHECK(material.getAmbientOcclusionMap()->getImage().getWidth() == 2);
+    CHECK(material.getAmbientOcclusionMap()->getImage().getHeight() == 2);
+
+    REQUIRE(material.getAmbientOcclusionMap()->getImage().getDataType() == Raz::ImageDataType::BYTE);
+
+    const auto* ambientOccData = static_cast<const uint8_t*>(material.getAmbientOcclusionMap()->getImage().getDataPtr());
+
+    // ---------
+    // | X |   |
+    // |-------|
+    // |   | X |
+    // ---------
+
+    CHECK(ambientOccData[0] == 0);
+    CHECK(ambientOccData[1] == 255);
+    CHECK(ambientOccData[2] == 255);
+    CHECK(ambientOccData[3] == 0);
   }
 }
 
