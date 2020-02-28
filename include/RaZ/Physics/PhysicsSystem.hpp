15a22df9f67603e171df7b1d18bede1eb4b6492d
Razakhel <romain.milbert@gmail.com>
2020-02-28 23:12:24 +0100

    [Physics/RigidBody] RigidBody now store bounciness instead of friction
    - Friction is part of the PhysicsSystem
    
    - Rigid bodies store their own inverse mass
      - This is a precalculation used in the system update
    
    - Defined RigodBody's ctor & functions as constexpr & noexcept

--- include/RaZ/Physics/PhysicsSystem.hpp
+++ include/RaZ/Physics/PhysicsSystem.hpp
@@ -13,12 +13,17 @@ public:
   PhysicsSystem();
 
   void setGravity(const Vec3f& gravity) { m_gravity = gravity; }
+  void setFriction(float friction) {
+    assert("Error: Friction coefficient must be between 0 & 1." && (friction >= 0.f && friction <= 1.f));
+    m_friction = friction;
+  }
 
   bool update(float deltaTime) override;
   void destroy() override {}
 
 private:
-  Vec3f m_gravity = Vec3f(0.f, -9.80665f, 0.f);
+  Vec3f m_gravity  = Vec3f(0.f, -9.80665f, 0.f); ///< Gravity force.
+  float m_friction = 0.95f; ///< Friction coefficient.
 };
 
 } // namespace Raz
