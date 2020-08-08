464ec4b5a1355db79bcd6f60cfe2aa66bc4de8ee
Razakhel <romain.milbert@gmail.com>
2020-08-08 23:19:19 +0200

    [Tests/Render/RenderGraph] Added a RenderGraph unit test file
    - This checks for the render graph's validity

---
+++ tests/RaZ/Render/RenderGraph.cpp
@@ -0,0 +1,24 @@
+#include "Catch.hpp"
+
+#include "RaZ/Render/RenderGraph.hpp"
+
+TEST_CASE("RenderGraph validity") {
+  Raz::RenderGraph graph;
+  CHECK(graph.isValid());
+
+  const Raz::Texture& depthTexture = graph.addTextureBuffer(1, 1, 0, Raz::ImageColorspace::DEPTH);
+  CHECK(graph.isValid());
+
+  // Adding a write texture to the geometry pass without an associated read texture: it is invalid
+  graph.getGeometryPass().addWriteTexture(depthTexture);
+  CHECK_FALSE(graph.isValid());
+
+  Raz::RenderPass& nextPass = graph.addNode();
+  CHECK_FALSE(graph.isValid()); // The next pass doesn't have any texture, the graph is still invalid
+
+  nextPass.addReadTexture(depthTexture, ""); // Unnecessary uniform name
+  CHECK_FALSE(graph.isValid()); // The geometry & the next pass have a matching texture buffer, but they aren't linked
+
+  graph.getGeometryPass().addChildren(nextPass);
+  CHECK(graph.isValid()); // The passes are linked & their buffers match, the graph is now valid
+}
