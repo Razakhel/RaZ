f2e0975c2a85a85f60f5957c4b775d39718c0ec5
Razakhel <romain.milbert@gmail.com>
2020-08-10 15:59:41 +0200

    [Utils/TypeUtils] Added an attribute detection implementation
    - This allows to check if a specific type has accessible attributes (constructors, operators, ...) & their return type
    
    - It is derived from the standard's TS std::experimental::is_detected
      - See: https://en.cppreference.com/w/cpp/experimental/is_detected
    
    - This currently does not work with MSVC

--- include/RaZ/Utils/TypeUtils.hpp
+++ include/RaZ/Utils/TypeUtils.hpp
@@ -77,6 +77,168 @@ constexpr std::string_view getTypeStr() noexcept {
 #endif
 }
 
+// Custom implementation of std::experimental::is_detected
+// See: https://en.cppreference.com/w/cpp/experimental/is_detected
+
+// TODO: this implementation doesn't work with MSVC (which always returns true for every attribute)
+// See: https://stackoverflow.com/a/35755737/3292304
+
+#if !defined(RAZ_COMPILER_MSVC)
+
+namespace Details {
+
+template <typename Default, typename AlwaysVoid, template <typename...> typename Attr, typename... Args>
+struct Detector {
+  using ValueT = std::false_type;
+  using Type   = Default;
+};
+
+template <typename Default, template <typename...> typename Attr, typename... Args>
+struct Detector<Default, std::void_t<Attr<Args...>>, Attr, Args...> {
+  using ValueT = std::true_type;
+  using Type   = Attr<Args...>;
+};
+
+struct Nonesuch {
+  Nonesuch(const Nonesuch&) = delete;
+  void operator=(const Nonesuch&) = delete;
+  ~Nonesuch() = delete;
+};
+
+template <template <typename...> typename Attr, typename... Args>
+using IsDetectedT = typename Details::Detector<Nonesuch, void, Attr, Args...>::ValueT;
+
+template <template <typename...> typename Attr, typename... Args>
+using DetectedT = typename Details::Detector<Nonesuch, void, Attr, Args...>::Type;
+
+template <typename Default, template <typename...> typename Attr, typename... Args>
+using DetectedOr = Details::Detector<Default, void, Attr, Args...>;
+
+template <typename Default, template <typename...> typename Attr, typename... Args>
+using DetectedOrT = typename DetectedOr<Default, Attr, Args...>::Type;
+
+template <typename Expected, template <typename...> typename Attr, typename... Args>
+using IsDetectedExactT = std::is_same<Expected, DetectedT<Attr, Args...>>;
+
+template <typename To, template <typename...> typename Attr, typename... Args>
+using IsDetectedConvertibleT = std::is_convertible<DetectedT<Attr, Args...>, To>;
+
+} // namespace Details
+
+namespace Attribute {
+
+template <typename T>
+using DefaultConstructor = decltype(T());
+
+template <typename T>
+using CopyConstructor = decltype(T(std::declval<const T&>()));
+
+template <typename T>
+using MoveConstructor = decltype(T(std::declval<T&&>()));
+
+template <typename T>
+using CopyAssignmentOperator = decltype(std::declval<T&>() = std::declval<const T&>());
+
+template <typename T>
+using MoveAssignmentOperator = decltype(std::declval<T&>() = std::declval<T&&>());
+
+template <typename T, typename U = T>
+using EqualityOperator = decltype(std::declval<T>() == std::declval<U>());
+
+template <typename T, typename U = T>
+using InequalityOperator = decltype(std::declval<T>() != std::declval<U>());
+
+template <typename T, typename U = T>
+using PlusOperator = decltype(std::declval<T>() + std::declval<U>());
+
+template <typename T, typename U = T>
+using MinusOperator = decltype(std::declval<T>() - std::declval<U>());
+
+template <typename T, typename U = T>
+using MultiplicationOperator = decltype(std::declval<T>() * std::declval<U>());
+
+template <typename T, typename U = T>
+using DivisionOperator = decltype(std::declval<T>() / std::declval<U>());
+
+template <typename T>
+using DefaultDestructor = decltype(std::declval<T&>().~T());
+
+} // namespace Attribute
+
+/// Checks if the given types make available an attribute.
+/// \note Non-accessible attributes (private/protected) cannot be detected.
+/// \tparam Attr Attribute to be checked.
+/// \tparam Args Types in which to check the attribute's existence.
+/// \return True if the attribute exists & is accessible, false otherwise.
+template <template <typename...> typename Attr, typename... Args>
+constexpr bool hasAttribute() { return Details::IsDetectedT<Attr, Args...>::value; }
+
+/// Checks if the given types' attribute returns the specific expected type.
+/// \tparam Expected Return type to be checked.
+/// \tparam Attr Attribute to be checked.
+/// \tparam Args Types for which to check the attribute's return type.
+/// \return True if the attribute's return type is the same as expected, false otherwise.
+template <typename Expected, template <typename...> typename Attr, typename... Args>
+constexpr bool hasReturnType() { return Details::IsDetectedExactT<Expected, Attr, Args...>::value; }
+
+/// Checks if the given types' attribute return type is convertible to the given one.
+/// \tparam To Convertible return type to be checked.
+/// \tparam Attr Attribute to be checked.
+/// \tparam Args Types for which to check the attribute's return type.
+/// \return True if the attribute's return type is convertible, false otherwise.
+template <typename To, template <typename...> typename Attr, typename... Args>
+constexpr bool hasReturnTypeConvertible() { return Details::IsDetectedConvertibleT<To, Attr, Args...>::value; }
+
+/// Checks if the default constructor is available for the given type.
+/// \tparam T Type to check the attribute for.
+/// \return True if the default constructor is available, false otherwise.
+template <typename T>
+constexpr bool hasDefaultConstructor() noexcept { return hasAttribute<Attribute::DefaultConstructor, T>(); }
+
+/// Checks if the copy constructor is available for the given type.
+/// \tparam T Type to check the attribute for.
+/// \return True if the copy constructor is available, false otherwise.
+template <typename T>
+constexpr bool hasCopyConstructor() noexcept { return hasAttribute<Attribute::CopyConstructor, T>(); }
+
+/// Checks if the move constructor is available for the given type.
+/// \tparam T Type to check the attribute for.
+/// \return True if the move constructor is available, false otherwise.
+template <typename T>
+constexpr bool hasMoveConstructor() noexcept { return hasAttribute<Attribute::MoveConstructor, T>(); }
+
+/// Checks if the copy assignment operator is available for the given type.
+/// \tparam T Type to check the attribute for.
+/// \return True if the copy assignment operator is available, false otherwise.
+template <typename T>
+constexpr bool hasCopyAssignmentOperator() noexcept { return hasAttribute<Attribute::CopyAssignmentOperator, T>(); }
+
+/// Checks if the move assignment operator is available for the given type.
+/// \tparam T Type to check the attribute for.
+/// \return True if the move assignment operator is available, false otherwise.
+template <typename T>
+constexpr bool hasMoveAssignmentOperator() noexcept { return hasAttribute<Attribute::MoveAssignmentOperator, T>(); }
+
+/// Checks if the equality operator is available for the given type.
+/// \tparam T Type to check the attribute for.
+/// \return True if the equality operator is available, false otherwise.
+template <typename T>
+constexpr bool hasEqualityOperator() noexcept { return hasAttribute<Attribute::EqualityOperator, T>(); }
+
+/// Checks if the inequality operator is available for the given type.
+/// \tparam T Type to check the attribute for.
+/// \return True if the inequality operator is available, false otherwise.
+template <typename T>
+constexpr bool hasInequalityOperator() noexcept { return hasAttribute<Attribute::InequalityOperator, T>(); }
+
+/// Checks if the default destructor is available for the given type.
+/// \tparam T Type to check the attribute for.
+/// \return True if the default destructor is available, false otherwise.
+template <typename T>
+constexpr bool hasDefaultDestructor() noexcept { return hasAttribute<Attribute::DefaultDestructor, T>(); }
+
+#endif
+
 } // namespace Raz::TypeUtils
 
 #endif // RAZ_TYPEUTILS_HPP
--- tests/RaZ/Utils/TypeUtils.cpp
+++ tests/RaZ/Utils/TypeUtils.cpp
@@ -2,6 +2,30 @@
 
 #include "RaZ/Utils/TypeUtils.hpp"
 
+namespace {
+
+class AttributeTest {
+public:
+  AttributeTest() = default;
+  AttributeTest(const AttributeTest&) = default;
+  AttributeTest(AttributeTest&&) = delete;
+
+  AttributeTest& operator=(const AttributeTest&) = default;
+  bool operator==(const AttributeTest&) { return true; }
+
+  operator bool() { return true; }
+
+  ~AttributeTest() = default;
+
+protected:
+  bool operator!=(const AttributeTest&) { return true; }
+
+private:
+  AttributeTest& operator=(AttributeTest&&) = default;
+};
+
+} // namespace
+
 TEST_CASE("TypeUtils type str") {
   int testInt {};
   const int& testIntRef = testInt;
@@ -26,3 +50,27 @@ TEST_CASE("TypeUtils type str") {
   CHECK(Raz::TypeUtils::getTypeStr<std::string_view>() == "class std::basic_string_view<char,struct std::char_traits<char> >");
 #endif
 }
+
+#if !defined(RAZ_COMPILER_MSVC)
+
+TEST_CASE("TypeUtils has attribute") {
+  CHECK(Raz::TypeUtils::hasDefaultConstructor<AttributeTest>());
+  CHECK(Raz::TypeUtils::hasCopyConstructor<AttributeTest>());
+  CHECK_FALSE(Raz::TypeUtils::hasMoveConstructor<AttributeTest>()); // Deleted
+
+  CHECK(Raz::TypeUtils::hasCopyAssignmentOperator<AttributeTest>());
+  CHECK_FALSE(Raz::TypeUtils::hasMoveAssignmentOperator<AttributeTest>()); // Existing but private
+
+  CHECK(Raz::TypeUtils::hasEqualityOperator<AttributeTest>());
+  CHECK_FALSE(Raz::TypeUtils::hasInequalityOperator<AttributeTest>()); // Existing but protected
+
+  CHECK(Raz::TypeUtils::hasDefaultDestructor<AttributeTest>());
+
+  CHECK(Raz::TypeUtils::hasReturnType<bool, Raz::TypeUtils::Attribute::EqualityOperator, AttributeTest>());
+  CHECK(Raz::TypeUtils::hasReturnType<AttributeTest&, Raz::TypeUtils::Attribute::CopyAssignmentOperator, AttributeTest>());
+
+  // AttributeTest has a non-explicit operator bool(); the created object is then implicitly convertible to bool
+  CHECK(Raz::TypeUtils::hasReturnTypeConvertible<bool, Raz::TypeUtils::Attribute::DefaultConstructor, AttributeTest>());
+}
+
+#endif
