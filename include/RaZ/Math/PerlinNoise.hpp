2632fe5f9704706c14c06f31632d9b227c086031
Razakhel <romain.milbert@gmail.com>
2021-04-30 17:12:08 +0200

    [Math/PerlinNoise] Added a 2D Perlin noise computation

---
+++ include/RaZ/Math/PerlinNoise.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef RAZ_PERLINNOISE_HPP
+#define RAZ_PERLINNOISE_HPP
+
+namespace Raz::PerlinNoise {
+
+float get(float x, float y);
+
+} // namespace Raz::PerlinNoise
+
+#endif // RAZ_PERLINNOISE_HPP
---
+++ tests/RaZ/Math/PerlinNoise.cpp
@@ -0,0 +1,13 @@
+#include "Catch.hpp"
+
+#include "RaZ/Math/PerlinNoise.hpp"
+
+TEST_CASE("Perlin noise 2D") {
+  CHECK(Raz::PerlinNoise::get(0.f, 0.f) == 0.5f);
+  CHECK(Raz::PerlinNoise::get(0.001f, 0.001f) == 0.49950001f);
+  CHECK(Raz::PerlinNoise::get(0.01f, 0.01f) == 0.49500701f);
+  CHECK(Raz::PerlinNoise::get(0.1f, 0.1f) == 0.45622027f);
+
+  CHECK(Raz::PerlinNoise::get(1.0123f, 2.0123f) == 0.50613433f);
+  CHECK(Raz::PerlinNoise::get(1.0123f, 2.0123f) == 0.50613433f); // The same coordinates must always give the same value
+}
