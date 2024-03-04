22f88f06f594905aaf4baa983d581d7e216273a3
Razakhel <romain.milbert@gmail.com>
2024-03-04 23:16:53 +0100

    [Extern/Tracy] Added Tracy 0.10 as an external
    - This will be used to instrument the code in order to profile applications

---
+++ extern/Tracy.cmake
@@ -0,0 +1,22 @@
+#########
+# Tracy #
+#########
+
+project(Tracy)
+
+add_library(Tracy STATIC)
+
+target_compile_features(Tracy PRIVATE cxx_std_17)
+
+target_sources(Tracy PRIVATE tracy/TracyClient.cpp)
+
+target_include_directories(Tracy SYSTEM PUBLIC tracy)
+
+target_compile_definitions(Tracy PUBLIC TRACY_ENABLE)
+
+# Disabling all compilers warnings
+if (RAZ_COMPILER_MSVC)
+    target_compile_options(Tracy PRIVATE /w)
+else ()
+    target_compile_options(Tracy PRIVATE -w)
+endif ()
