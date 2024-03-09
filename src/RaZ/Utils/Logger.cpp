1d128a898c18e287a10415f7d0bef18dcd83626b
Razakhel <romain.milbert@gmail.com>
2024-03-10 00:10:24 +0100

    [Utils/Logger] Added Tracy messages to logging functions
    - The error one recovers part of the callstack to get a precise view of what could have failed

--- src/RaZ/Utils/Logger.cpp
+++ src/RaZ/Utils/Logger.cpp
@@ -1,5 +1,7 @@
 #include "RaZ/Utils/Logger.hpp"
 
+#include "tracy/Tracy.hpp"
+
 #include <iostream>
 
 namespace Raz {
@@ -7,6 +9,8 @@ namespace Raz {
 void Logger::error(const std::string& message) {
   if (static_cast<int>(m_logLevel) < static_cast<int>(LoggingLevel::ERROR))
     return;
+
+  TracyMessageCS(message.c_str(), message.size(), tracy::Color::Red, 10);
 
   if (m_logFunc)
     m_logFunc(LoggingLevel::ERROR, message);
@@ -18,6 +22,8 @@ void Logger::warn(const std::string& message) {
   if (static_cast<int>(m_logLevel) < static_cast<int>(LoggingLevel::WARNING))
     return;
 
+  TracyMessageC(message.c_str(), message.size(), tracy::Color::Gold);
+
   if (m_logFunc)
     m_logFunc(LoggingLevel::WARNING, message);
   else
@@ -27,6 +33,8 @@ void Logger::warn(const std::string& message) {
 void Logger::info(const std::string& message) {
   if (static_cast<int>(m_logLevel) < static_cast<int>(LoggingLevel::INFO))
     return;
+
+  TracyMessageC(message.c_str(), message.size(), tracy::Color::DeepSkyBlue);
 
   if (m_logFunc)
     m_logFunc(LoggingLevel::INFO, message);
@@ -38,6 +46,8 @@ void Logger::info(const std::string& message) {
 void Logger::debug(const std::string& message) {
   if (static_cast<int>(m_logLevel) < static_cast<int>(LoggingLevel::DEBUG))
     return;
+
+  TracyMessageC(message.c_str(), message.size(), tracy::Color::Gray);
 
   if (m_logFunc)
     m_logFunc(LoggingLevel::DEBUG, message);
