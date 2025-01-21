40d66e1f1f129168d8744ba566b994192b32944d
Razakhel <romain.milbert@gmail.com>
2025-01-21 22:55:56 +0100

    [Utils/Memory] Added memory profiling capabilities
    - Overloaded most of the new & delete operators to hook Tracy's memory profiling

---
+++ src/RaZ/Utils/Memory.cpp
@@ -0,0 +1,69 @@
+#include <tracy/Tracy.hpp>
+
+// See https://en.cppreference.com/w/cpp/memory/new/operator_new
+
+void* operator new(std::size_t size) {
+  size = std::max(static_cast<std::size_t>(1), size);
+
+  if (void* ptr = std::malloc(size)) {
+    TracyAlloc(ptr, size);
+    return ptr;
+  }
+
+  throw std::bad_alloc();
+}
+
+void* operator new(std::size_t size, const std::nothrow_t&) noexcept {
+  try {
+      return operator new(size);
+  } catch (...) {
+      return nullptr;
+  }
+}
+
+void* operator new[](std::size_t size) {
+  size = std::max(static_cast<std::size_t>(1), size);
+
+  if (void* ptr = std::malloc(size)) {
+    TracyAlloc(ptr, size);
+    return ptr;
+  }
+
+  throw std::bad_alloc();
+}
+
+void* operator new[](std::size_t size, const std::nothrow_t&) noexcept {
+  try {
+      return operator new[](size);
+  } catch (...) {
+      return nullptr;
+  }
+}
+
+// See https://en.cppreference.com/w/cpp/memory/new/operator_delete
+
+void operator delete(void* ptr) noexcept {
+  TracyFree(ptr);
+  std::free(ptr);
+}
+
+void operator delete(void* ptr, std::size_t) noexcept {
+  operator delete(ptr);
+}
+
+void operator delete(void* ptr, const std::nothrow_t&) noexcept {
+  operator delete(ptr);
+}
+
+void operator delete[](void* ptr) noexcept {
+  TracyFree(ptr);
+  std::free(ptr);
+}
+
+void operator delete[](void* ptr, std::size_t) noexcept {
+  operator delete[](ptr);
+}
+
+void operator delete[](void* ptr, const std::nothrow_t&) noexcept {
+  operator delete[](ptr);
+}
